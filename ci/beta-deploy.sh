250e733a04027be4d6fc94fe627bccf8c95a117e
Davide De Rosa <keeshux@gmail.com>
2019-03-31 21:26:51 +0200

    Only tag after successful deploy

--- ci/beta-deploy.sh
+++ ci/beta-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-TARGET="beta" bundle exec fastlane --env secret-deploy,beta-deploy beta_deploy
 VERSION=`agvtool mvers -terse1`
 BUILD=`agvtool vers -terse`
-git tag "v$VERSION-b$BUILD"
+TARGET="beta" bundle exec fastlane --env secret-deploy,beta-deploy beta_deploy && git tag "v$VERSION-b$BUILD"
--- ci/dev-deploy.sh
+++ ci/dev-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-TARGET="dev" bundle exec fastlane --env secret-deploy,dev-deploy dev_deploy
 VERSION=`agvtool mvers -terse1`
 BUILD=`agvtool vers -terse`
-#git tag "v$VERSION-a$BUILD"
+TARGET="dev" bundle exec fastlane --env secret-deploy,dev-deploy dev_deploy #&& git tag "v$VERSION-a$BUILD"
