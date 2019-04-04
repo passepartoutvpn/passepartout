68f7046304127b05940ecdcc265609d4a903b225
Davide De Rosa <keeshux@gmail.com>
2019-04-05 01:06:54 +0200

    Copy solid snapshots
    GitHub doesn't handle symlinks as expected.

---
+++ ci/copy-readme-snapshots.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -f res/snap-*.png
+cp fastlane/screenshots/en-US/iphone65-02.png res/snap-home.png
+cp fastlane/screenshots/en-US/iphone65-04.png res/snap-parameters.png
+cp fastlane/screenshots/en-US/iphone65-03.png res/snap-profile.png
+cp fastlane/screenshots/en-US/iphone65-05.png res/snap-trusted.png
