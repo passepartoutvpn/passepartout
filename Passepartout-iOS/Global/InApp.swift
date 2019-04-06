895c19328e8d0c3e3872a8497353a1cda491be32
Davide De Rosa <keeshux@gmail.com>
2019-04-06 23:50:07 +0200

    Add in-app donation identifiers

---
+++ Passepartout-iOS/Global/InApp.swift
@@ -0,0 +1,42 @@
+//
+//  InApp.swift
+//  Passepartout-iOS
+//
+//  Created by Davide De Rosa on 4/6/19.
+//  Copyright (c) 2019 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+struct InApp {
+    struct Donations {
+        static let tiny = "com.algoritmico.ios.Passepartout.donations.Tiny"
+
+        static let small = "com.algoritmico.ios.Passepartout.donations.Small"
+
+        static let medium = "com.algoritmico.ios.Passepartout.donations.Medium"
+
+        static let big = "com.algoritmico.ios.Passepartout.donations.Big"
+
+        static let huge = "com.algoritmico.ios.Passepartout.donations.Huge"
+
+        static let maxi = "com.algoritmico.ios.Passepartout.donations.Maxi"
+    }
+}
