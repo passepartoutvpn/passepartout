00aedbf4704f45daeeedcb2c3abcbc3b174dab98
Davide De Rosa <keeshux@gmail.com>
2019-04-07 17:48:23 +0200

    Update changelog and release notes at once

--- ci/commit-release-notes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TARGET="fastlane/metadata/en-US/release_notes.txt"
-ci/latest-changelog.sh | sed -E "s/^(.*) \[#.*$/\1/" >$TARGET
-git add $TARGET
-git commit -m "Update release notes (en)"
