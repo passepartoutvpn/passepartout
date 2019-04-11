1d91e484b929d9571f88b18f3583598da74a3808
Davide De Rosa <keeshux@gmail.com>
2019-04-11 14:30:15 +0200

    Add parameter to list untranslated

--- ci/list-untranslated.sh
+++ ci/list-untranslated.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 DIR="Passepartout/Resources"
 FILENAME="Localizable.strings"
-STRINGS_EN="$DIR/en.lproj/$FILENAME"
-STRINGS_IT="$DIR/it.lproj/$FILENAME"
+LANG_BASE="en"
+LANG_TARGET="$1"
+STRINGS_BASE="$DIR/$LANG_BASE.lproj/$FILENAME"
+STRINGS_TARGET="$DIR/$LANG_TARGET.lproj/$FILENAME"
 IDS="string-ids.tmp"
 
-sed -E "s/^(.+) = .*$/\1/" $STRINGS_EN | grep '^"' >$IDS.en
-sed -E "s/^(.+) = .*$/\1/" $STRINGS_IT | grep '^"' >$IDS.it
-diff $IDS.en $IDS.it | grep "^<" | sed -E 's/^< "(.*)"$/\1/g'
+sed -E "s/^(.+) = .*$/\1/" $STRINGS_BASE | grep '^"' >$IDS.$LANG_BASE
+sed -E "s/^(.+) = .*$/\1/" $STRINGS_TARGET | grep '^"' >$IDS.$LANG_TARGET
+diff $IDS.$LANG_BASE $IDS.$LANG_TARGET | grep "^<" | sed -E 's/^< "(.*)"$/\1/g'
 rm -f $IDS.*
