990654287b54cebb147bf6ceb1ffbdfb209642f3
Davide De Rosa <keeshux@gmail.com>
2019-04-13 19:27:11 +0200

    Show English version of untranslated strings

--- ci/list-untranslated.sh
+++ ci/list-untranslated.sh
@@ -6,8 +6,12 @@ LANG_TARGET="$1"
 STRINGS_BASE="$DIR/$LANG_BASE.lproj/$FILENAME"
 STRINGS_TARGET="$DIR/$LANG_TARGET.lproj/$FILENAME"
 IDS="string-ids.tmp"
+TMPOUT="untranslated.tmp"
 
 sed -E "s/^(.+) = .*$/\1/" $STRINGS_BASE | grep '^"' >$IDS.$LANG_BASE
 sed -E "s/^(.+) = .*$/\1/" $STRINGS_TARGET | grep '^"' >$IDS.$LANG_TARGET
-diff $IDS.$LANG_BASE $IDS.$LANG_TARGET | grep "^<" | sed -E 's/^< "(.*)"$/\1/g'
+diff $IDS.$LANG_BASE $IDS.$LANG_TARGET | grep "^<" | sed -E 's/^< "(.*)"$/\1/g' >$TMPOUT
 rm -f $IDS.*
+
+grep -f $TMPOUT $STRINGS_BASE
+rm $TMPOUT
