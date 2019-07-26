fd3731e2bce7d71d2f86dac912758fc7cf3c8e43
Davide De Rosa <keeshux@gmail.com>
2019-07-27 00:23:31 +0200

    Fix untranslated strings checker

--- ci/list-untranslated.sh
+++ ci/list-untranslated.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-DIR="Passepartout-iOS/Global"
-FILENAME="App.strings"
+DIR_APP="Passepartout-iOS/Global"
+FILENAME_APP="App.strings"
+DIR_CORE="Submodules/Core/Passepartout/Resources"
+FILENAME_CORE="Core.strings"
+
+DIR=$DIR_APP
+FILENAME=$FILENAME_APP
 LANG_BASE="en"
 LANG_TARGET="$1"
 STRINGS_BASE="$DIR/$LANG_BASE.lproj/$FILENAME"
@@ -8,8 +13,8 @@ STRINGS_TARGET="$DIR/$LANG_TARGET.lproj/$FILENAME"
 IDS="string-ids.tmp"
 TMPOUT="untranslated.tmp"
 
-sed -E "s/^(.+) = .*$/\1/" $STRINGS_BASE | grep '^"' >$IDS.$LANG_BASE
-sed -E "s/^(.+) = .*$/\1/" $STRINGS_TARGET | grep '^"' >$IDS.$LANG_TARGET
+sed -E "s/^(.+) = .*$/\1/" $STRINGS_BASE | sort | grep '^"' >$IDS.$LANG_BASE
+sed -E "s/^(.+) = .*$/\1/" $STRINGS_TARGET | sort | grep '^"' >$IDS.$LANG_TARGET
 diff $IDS.$LANG_BASE $IDS.$LANG_TARGET | grep "^<" | sed -E 's/^< "(.*)"$/\1/g' >$TMPOUT
 rm -f $IDS.*
 
