cc786df294b3e991e1a7d4c2ecb2f86df55a9f1a
Davide De Rosa <keeshux@gmail.com>
2019-12-12 20:55:48 +0100

    Move copy release notes script out

--- scripts/copy-release-notes.sh
+++ scripts/copy-release-notes.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-METADATA="release_notes"
-RX='^[a-z]{2}(\-[A-z]+)?$'
-cd fastlane/metadata
-for LANG in `ls -d *`; do
-    if [[ $LANG == "en-US" ]]; then
-        continue
-    fi
-    if [[ ! $LANG =~ $RX ]]; then
-        continue
-    fi
-    #echo $LANG
-    cp en-US/$METADATA.txt $LANG
-done
+../Submodules/Core/scripts/copy-release-notes.sh
