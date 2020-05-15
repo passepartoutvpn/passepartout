9282525be0f205f8adcd976096effd476fb3d8f9
Davide De Rosa <keeshux@gmail.com>
2020-05-15 13:24:52 +0200

    Add scripts to add provider to store description
    Should also add before (for first provider), not only after.

---
+++ scripts/add-provider-description.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PATTERN="fastlane/metadata/*/description.txt"
+AFTER=$1
+NAME=$2
+DELIMITER="†"
+
+for FILE in `ls $PATTERN`; do
+    sed -E "s/${AFTER}/${AFTER}${DELIMITER}- ${NAME}/g" $FILE | tr $DELIMITER '\n' >$FILE.tmp
+    mv $FILE.tmp $FILE
+done
