750819845587928e469e644a61fcb379650d0f80
Davide De Rosa <keeshux@gmail.com>
2021-01-06 19:05:13 +0100

    Submit iOS build to TestFlight

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ci/store-deploy.sh mac || { echo "Failed to deploy macOS" ; exit 1 ; }
-ci/store-deploy.sh ios || { echo "Failed to deploy iOS" ; exit 1 ; }
+ci/beta-deploy.sh ios && || { echo "Failed to deploy iOS" ; exit 1 ; }
