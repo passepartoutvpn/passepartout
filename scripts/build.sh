35b1bfdfbed53e0b8ef6c34ae8bac9af7c61496d
Davide De Rosa <keeshux@gmail.com>
2021-01-30 21:56:23 +0100

    Do not reset after build
    For proper tagging on submit.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ scripts/commit-beta.sh
 ci/beta-archive.sh mac || { echo "Failed to build macOS" ; exit 1 ; }
 scripts/reset-archive.sh
 ci/beta-archive.sh ios || { echo "Failed to build iOS" ; exit 1 ; }
-scripts/reset-archive.sh
+#scripts/reset-archive.sh
