52b3d3129913f63c35d5b8874d0f9e2c8633c878
Davide De Rosa <keeshux@gmail.com>
2021-07-30 17:27:42 +0200

    Fix bogus country codes in ProtonVPN

---
+++ Passepartout/Core/Sources/Services/Infrastructure+CredentialsPurpose.swift
@@ -0,0 +1,9 @@
+//
+//  Infrastructure+CredentialsPurpose.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 30/07/21.
+//  Copyright © 2021 Davide De Rosa. All rights reserved.
+//
+
+import Foundation
