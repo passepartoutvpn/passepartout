f22916f1d0f5326497625756f1c86820066d4fce
Davide De Rosa <keeshux@gmail.com>
2021-09-27 19:33:22 +0200

    Deploy beta via GitHub Actions
    - Disassemble deploy scripts inside workflow
    - Update gitignore to avoid dirty status
    - No readonly
    - Create additional certs for macOS

--- scripts/build-appcenter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf build dist
-scripts/commit-beta.sh
-ci/dev-archive.sh mac || { echo "Failed to build macOS" ; exit 1 ; }
-scripts/reset-archive.sh
-ci/dev-archive.sh ios || { echo "Failed to build iOS" ; exit 1 ; }
-#scripts/reset-archive.sh
--- scripts/submit-appcenter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ci/dev-deploy.sh mac || { echo "Failed to deploy macOS" ; exit 1 ; }
-ci/dev-deploy.sh ios || { echo "Failed to deploy iOS" ; exit 1 ; }
-
-VERSION=`agvtool mvers -terse1`
-BUILD=`agvtool vers -terse`
-git tag "v$VERSION-a$BUILD"
-
-scripts/reset-archive.sh
