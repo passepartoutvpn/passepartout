43d11e20cc5a403305e2e516bc5e678eec1c6739
Davide De Rosa <keeshux@gmail.com>
2021-09-29 15:09:15 +0200

    Add script to bootstrap new version

---
+++ scripts/bootstrap-version.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+VERSION=$1
+
+if [[ -z $VERSION ]]; then
+    echo "Version number required"
+    exit 1
+fi
+
+CHANGELOG_GLOB="Passepartout/App/*/CHANGELOG.md"
+
+ci/set-version.sh $VERSION
+sed -i '' -E "s/(^and this project adheres.*$)/\1\n\n## Unreleased/" $CHANGELOG_GLOB
+
+git add *.plist $CHANGELOG_GLOB
+git commit -m "Bump version"
