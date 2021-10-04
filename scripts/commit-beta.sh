0a6894d82ca193dc4e2bac1ee467a9e213ce2759
Davide De Rosa <keeshux@gmail.com>
2021-10-04 19:21:14 +0200

    Rewrite beta local/remote flow
    - Merge envs
    - Explicit lanes in workflow

--- scripts/commit-beta.sh
+++ scripts/commit-beta.sh
@@ -7,15 +7,13 @@ fi
 
 ci/update-changelog.sh ios
 ci/update-changelog.sh mac
+ci/copy-release-notes.sh ios
+ci/copy-release-notes.sh mac
+
 git add Passepartout/App/*/CHANGELOG.md
 git add Passepartout/App/*/fastlane/metadata/*/release_notes.txt
 git commit -m "Set beta release"
 
-VERSION=`agvtool mvers -terse1`
-BUILD=`agvtool vers -terse`
-
-# predict build number (add commits count)
-HISTORY=`git rev-list --count HEAD`
-BUILD=$((BUILD + HISTORY))
-
+VERSION=`ci/version-number.sh`
+BUILD=`ci/build-number.sh`
 git tag "v$VERSION-b$BUILD"
--- scripts/push-refs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git push && git push github
-git push --tags && git push --tags github
-git checkout stable && git merge master
-git push github
-git checkout master
