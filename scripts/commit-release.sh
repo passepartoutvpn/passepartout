d629540c1856d622e426ee7c7fa85268d6180934
Davide De Rosa <keeshux@gmail.com>
2021-10-06 13:17:07 +0200

    Automate release tag message

--- scripts/commit-release.sh
+++ scripts/commit-release.sh
@@ -8,5 +8,6 @@ fi
 VERSION=`ci/version-number.sh`
 DATE=`date "+%Y-%m-%d"`
 CHANGELOG_GLOB="Passepartout/App/*/CHANGELOG.md"
+MESSAGE="Release"
 sed -i '' -E "s/^.*Beta.*$/## $VERSION ($DATE)/" $CHANGELOG_GLOB
-git commit -am "Release" && git tag -as "v$VERSION"
+git commit -am $MESSAGE && git tag -as "v$VERSION" -m $MESSAGE
