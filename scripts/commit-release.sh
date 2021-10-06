9ca015e42f6807f7e8b7ae8be3675eda46e0c06f
Davide De Rosa <keeshux@gmail.com>
2021-10-06 13:22:45 +0200

    Roll back release on git errors

--- scripts/commit-release.sh
+++ scripts/commit-release.sh
@@ -10,4 +10,15 @@ DATE=`date "+%Y-%m-%d"`
 CHANGELOG_GLOB="Passepartout/App/*/CHANGELOG.md"
 MESSAGE="Release"
 sed -i '' -E "s/^.*Beta.*$/## $VERSION ($DATE)/" $CHANGELOG_GLOB
-git commit -am $MESSAGE && git tag -as "v$VERSION" -m $MESSAGE
+
+if ! git commit -am $MESSAGE; then
+    echo "Failed to commit release"
+    git reset --hard
+    exit
+fi
+
+if ! git tag -as "v$VERSION" -m $MESSAGE; then
+    echo "Failed to tag release"
+    git reset --hard HEAD^
+    exit
+fi
