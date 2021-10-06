f63b6793491fa7aca5a909e6e31397a7a16d04cd
Davide De Rosa <keeshux@gmail.com>
2021-10-06 13:13:36 +0200

    Restrict release to stable branch

--- scripts/commit-release.sh
+++ scripts/commit-release.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 CURRENT_BRANCH=`git branch --show-current`
-if [[ $CURRENT_BRANCH != "master" ]]; then
-    echo "Not on master branch"
+if [[ $CURRENT_BRANCH != "stable" ]]; then
+    echo "Not on stable branch"
     exit
 fi
 
 VERSION=`ci/version-number.sh`
 DATE=`date "+%Y-%m-%d"`
 CHANGELOG_GLOB="Passepartout/App/*/CHANGELOG.md"
-sed -i '' -E "s/^.*Beta.*$/## $VERSION ($DATE)/g" $CHANGELOG_GLOB
+sed -i '' -E "s/^.*Beta.*$/## $VERSION ($DATE)/" $CHANGELOG_GLOB
 git commit -am "Release" && git tag -as "v$VERSION"
