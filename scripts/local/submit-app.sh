fd918aa4a2f0871b211f280b342548e48c6f10b0
Davide De Rosa <keeshux@gmail.com>
2021-11-27 13:49:23 +0100

    Fix missing .ipa/.pkg in local script

--- scripts/local/submit-app.sh
+++ scripts/local/submit-app.sh
@@ -4,5 +4,10 @@ if [[ -z $PLATFORM ]]; then
     echo "Platform required"
     exit
 fi
+if [[ $PLATFORM == "ios" ]]; then
+    export PILOT_IPA="dist/ios/Passepartout.ipa"
+else
+    export PILOT_PKG="dist/mac/Passepartout.pkg"
+fi
 export PILOT_CHANGELOG=`ci/build-changelog.sh $PLATFORM`
 bundle exec fastlane --env $PLATFORM,beta,secret store_beta
