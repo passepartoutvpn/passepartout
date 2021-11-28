51821660d24aae2d8ca4379d4e84a42fa4768d01
Davide De Rosa <keeshux@gmail.com>
2021-11-28 22:46:35 +0100

    Commit build number with beta
    Pick base build number from env, not project. Can now drop
    fetch-depth 0 from workflow, no needs for commits count.

--- scripts/commit-beta.sh
+++ scripts/commit-beta.sh
@@ -5,17 +5,32 @@ if [[ $CURRENT_BRANCH != "master" ]]; then
     exit
 fi
 
+# just in case
+scripts/after-beta.sh
+
+# set build number
+. .env
+BUILD=$((BASE_BUILD_NUMBER + `git rev-list HEAD --count` + 1))
+ci/set-build.sh $BUILD
+
+# set release notes
 ci/update-changelog.sh ios &&
     ci/update-changelog.sh mac &&
     ci/copy-release-notes.sh ios &&
     ci/copy-release-notes.sh mac
 
+# pull latest API
 git -C PassepartoutCore/Sources/PassepartoutCore/API pull origin master
 git add */PassepartoutCore/API
+
+# add build number
+git add Passepartout.xcodeproj
+git add *.plist
+
+# add release notes
 git add Passepartout/App/*/CHANGELOG.md
 git add Passepartout/App/*/fastlane/metadata/*/release_notes.txt
-git commit -m "Set beta release"
 
-VERSION=`ci/version-number.sh ios`
-BUILD=`ci/build-number.sh ios`
-git tag "v$VERSION-b$BUILD"
+git commit -m "Set beta release"
+#VERSION=`ci/version-number.sh ios`
+#git tag "v$VERSION-b$BUILD"
