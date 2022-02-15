a15abe94db43147abc7233168085189680cbd739
Davide De Rosa <keeshux@gmail.com>
2022-02-15 08:56:35 +0100

    [ci skip] Use versioned TunnelKit

--- PassepartoutCore/Package.swift
+++ PassepartoutCore/Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", from: "4.0.0"),
-        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", .revision("871e51517c5678d9c683104bd6b0617d5eb2641e")),
+        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", from: "4.1.0"),
+//        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", .revision("871e51517c5678d9c683104bd6b0617d5eb2641e")),
 //        .package(name: "TunnelKit", path: "../../tunnelkit"),
         .package(name: "Convenience", url: "https://github.com/keeshux/convenience", .revision("347105ec0ce27cd4255acf9875fd60ad1f213801")),
         .package(url: "https://github.com/Cocoanetics/Kvitto", from: "1.0.0")
