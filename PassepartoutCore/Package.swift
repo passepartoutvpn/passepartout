dfebb60796d29d14128de2888d94eaafdf413ac1
Davide De Rosa <keeshux@gmail.com>
2022-04-13 22:41:12 +0200

    Fix an import issue in TunnelKit

--- PassepartoutCore/Package.swift
+++ PassepartoutCore/Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 //        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", from: "4.1.0"),
-        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", .revision("02e702d97ba495edc6533edd41eb482be9bb3311")),
+        .package(name: "TunnelKit", url: "https://github.com/passepartoutvpn/tunnelkit", .revision("07e94b2d5cea3513c11e29576851dcc9baa15e13")),
 //        .package(name: "TunnelKit", path: "../../tunnelkit"),
         .package(url: "https://github.com/zoul/generic-json-swift", from: "2.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0")
