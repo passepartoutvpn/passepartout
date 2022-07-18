b26e334881098f43e31a9a3367d8503a1b3f4047
Davide De Rosa <keeshux@gmail.com>
2022-07-18 02:40:44 +0200

    Get Mac strings from main bundle
    Alter bundle in SwiftGen configuration file.

--- Passepartout/AppShared/Constants/SwiftGen+Strings.swift
+++ Passepartout/AppShared/Constants/SwiftGen+Strings.swift
@@ -1030,19 +1030,7 @@ internal enum L10n {
 
 extension L10n {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-    let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+    let format = Bundle.main.localizedString(forKey: key, value: nil, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
   }
 }
-
-// swiftlint:disable convenience_type
-private final class BundleToken {
-  static let bundle: Bundle = {
-    #if SWIFT_PACKAGE
-    return Bundle.module
-    #else
-    return Bundle(for: BundleToken.self)
-    #endif
-  }()
-}
-// swiftlint:enable convenience_type
