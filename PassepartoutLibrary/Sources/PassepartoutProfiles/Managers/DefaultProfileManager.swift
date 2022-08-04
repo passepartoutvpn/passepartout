passepartoutvpn/passepartout

commit dec7fb9030d73e99d0d25cd753ca87115c4ec897

Activate first created profile

PassepartoutLibrary/Sources/PassepartoutProfiles/Managers/DefaultProfileManager.swift
@@ -175,18 +175,18 @@ extension DefaultProfileManager {
             assertionFailure("Placeholder")
             return
         }
+        
+        let isActive = isActive ?? allHeaders.isEmpty
 
         pp_log.info("Writing profile \(profile.logDescription) to persistent store")
         strategy.saveProfile(profile)
 
-        if let isActive = isActive {
-            if isActive {
-                pp_log.info("\tActivating profile...")
-                activeProfileId = profile.id
-            } else if activeProfileId == profile.id {
-                pp_log.info("\tDeactivating profile...")
-                activeProfileId = nil
-            }
+        if isActive {
+            pp_log.info("\tActivating profile...")
+            activeProfileId = profile.id
+        } else if activeProfileId == profile.id {
+            pp_log.info("\tDeactivating profile...")
+            activeProfileId = nil
         }
 
         // IMPORTANT: refresh live copy if just saved (e.g. via intents)

commit b183dece8c73685efff9a22a6b110642f31e8c8a

Fix logic to activate first created profile
Broken in dec7fb9030d73e99d0d25cd753ca87115c4ec897

VPN was lost when app was sent to background. Current (and active)
profile was persisted with nil isActive and instead of ignoring
profile activation, isActive was falling back to
allHeaders.isEmpty (false), erroneously deactivating the profile
and disconnecting the VPN.

PassepartoutLibrary/Sources/PassepartoutProfiles/Managers/DefaultProfileManager.swift
@@ -175,18 +175,21 @@ extension DefaultProfileManager {
             assertionFailure("Placeholder")
             return
         }
-        
-        let isActive = isActive ?? allHeaders.isEmpty
 
         pp_log.info("Writing profile \(profile.logDescription) to persistent store")
         strategy.saveProfile(profile)
 
-        if isActive {
-            pp_log.info("\tActivating profile...")
+        if let isActive = isActive {
+            if isActive {
+                pp_log.info("\tActivating profile...")
+                activeProfileId = profile.id
+            } else if activeProfileId == profile.id {
+                pp_log.info("\tDeactivating profile...")
+                activeProfileId = nil
+            }
+        } else if allHeaders.isEmpty {
+            pp_log.info("\tActivating first profile...")
             activeProfileId = profile.id
-        } else if activeProfileId == profile.id {
-            pp_log.info("\tDeactivating profile...")
-            activeProfileId = nil
         }
 
         // IMPORTANT: refresh live copy if just saved (e.g. via intents)