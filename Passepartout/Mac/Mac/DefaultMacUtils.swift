3b146b3e0f83898226467dd6c9f60b4a236093db
Davide De Rosa <keeshux@gmail.com>
2022-09-05 07:58:20 +0200

    Fix var syntax

--- Passepartout/Mac/Mac/DefaultMacUtils.swift
+++ Passepartout/Mac/Mac/DefaultMacUtils.swift
@@ -27,7 +27,7 @@ import Foundation
 import AppKit
 
 class DefaultMacUtils: MacUtils {
-    lazy private(set) var isStartedByLauncher = NSApp.isHidden
+    private(set) lazy var isStartedByLauncher = NSApp.isHidden
     
     private let transformer = ObservableProcessTransformer.shared
     
