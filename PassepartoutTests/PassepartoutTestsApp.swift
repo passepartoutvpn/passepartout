2c46c5cadfd01d2d9762e36b13c607d88e465ff8
Davide De Rosa <keeshux@gmail.com>
2022-10-30 11:41:34 +0100

    Delete whole test target skeleton

--- PassepartoutTests/PassepartoutTestsApp.swift
+++ PassepartoutTests/PassepartoutTestsApp.swift
@@ -1,9 +1,26 @@
 //
 //  PassepartoutTestsApp.swift
-//  PassepartoutTests
+//  Passepartout
 //
-//  Created by Davide De Rosa on 30/10/22.
-//  Copyright © 2022 Davide De Rosa. All rights reserved.
+//  Created by Davide De Rosa on 10/30/22.
+//  Copyright (c) 2022 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import SwiftUI
@@ -12,7 +29,7 @@ import SwiftUI
 struct PassepartoutTestsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmptyView()
         }
     }
 }
