12e0b9fdd176eca9d4b46ba6c4b844aa2b9a2812
Davide De Rosa <keeshux@gmail.com>
2024-01-05 16:11:16 +0100

    Fix l10n scripts
    - Ukranian missing from import
    - Delete obsolete export

--- scripts/util/import-onesky-translations.sh
+++ scripts/util/import-onesky-translations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SRC="$1"
 DST="$2"
-for LANG in "de" "el" "en" "es" "fr" "it" "nl" "pl" "pt" "ru" "sv" "zh-Hans"; do
+for LANG in "de" "el" "en" "es" "fr" "it" "nl" "pl" "pt" "ru" "sv" "uk" "zh-Hans"; do
     cp $SRC/$LANG/* $DST/$LANG.lproj
 done
