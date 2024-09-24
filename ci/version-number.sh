f2536b45b71d452326730b287cc5fb1a5f1c6802
Davide De Rosa <keeshux@gmail.com>
2024-09-24 17:25:03 +0200

    Pass xcodeproj path as an argument

--- ci/version-number.sh
+++ ci/version-number.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-XCODEPROJ="$(dirname "$0")/../Passepartout.xcodeproj/project.pbxproj"
+if [ -z "$1" ]; then
+    echo "Path to Xcode project required"
+    exit 1
+fi
+XCODEPROJ="$1"
 grep MARKETING_VERSION $XCODEPROJ | uniq | sed -E "s/^.*MARKETING_VERSION = ([0-9]\.[0-9]\.[0-9]);/\1/" | tr -d '\n'
