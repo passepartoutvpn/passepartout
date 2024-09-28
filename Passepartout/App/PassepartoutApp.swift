passepartoutvpn/passepartout

commit 9e6e59276b9d1ec82e98375dbba4603f428d7d05

Associate and handle imported files (#628)

Passepartout/App/PassepartoutApp.swift
@@ -42,11 +42,13 @@ struct PassepartoutApp: App {
     private let context: AppContext = .shared
 //    private let context: AppContext = .mock(withRegistry: .shared)
 
+    private let appName = Constants.shared.identifiers.displayName
+
     @StateObject
     private var theme = Theme()
 
     var body: some Scene {
-        WindowGroup {
+        Window(appName, id: appName) {
             AppCoordinator(
                 profileManager: context.profileManager,
                 tunnel: context.tunnel,

Passepartout/App/App.plist
@@ -32,8 +32,7 @@
 			<string>Alternate</string>
 			<key>LSItemContentTypes</key>
 			<array>
-				<string>public.text</string>
-				<string>public.zip-archive</string>
+				<string>public.item</string>
 			</array>
 		</dict>
 		<dict>

Passepartout/Library/Sources/AppUI/Views/UI/ProfileImporterModifier.swift
@@ -47,8 +47,11 @@ struct ProfileImporterModifier: ViewModifier {
                 isPresented: $isPresented,
                 allowedContentTypes: [.item],
                 allowsMultipleSelection: true,
-                onCompletion: onCompletion
+                onCompletion: handleResult
             )
+            .onOpenURL { url in
+                handleResult(.success([url]))
+            }
             .alert(
                 Strings.Views.Profiles.Toolbar.importProfile,
                 isPresented: $importer.isPresentingPassphrase,
@@ -85,7 +88,7 @@ private extension ProfileImporterModifier {
         Text(Strings.Views.Profiles.Alerts.Import.Passphrase.message(url.lastPathComponent))
     }
 
-    func onCompletion(_ result: Result<[URL], Error>) {
+    func handleResult(_ result: Result<[URL], Error>) {
         Task.detached {
             do {
                 let urls = try result.get()

commit c0234b07a895189c11ebb2dd02da3016038d20e8

Fix missing Window on iOS
Broken by 9e6e59276b9d1ec82e98375dbba4603f428d7d05

Passepartout/App/PassepartoutApp.swift
@@ -48,26 +48,32 @@ struct PassepartoutApp: App {
     private var theme = Theme()
 
     var body: some Scene {
-        Window(appName, id: appName) {
-            AppCoordinator(
-                profileManager: context.profileManager,
-                tunnel: context.tunnel,
-                registry: context.registry
-            )
-            .onLoad {
-                CommonLibrary.configureLogging(
-                    to: Constants.shared.urlForAppLog,
-                    parameters: Constants.shared.log
-                )
-                AppUI.configure(with: context)
-            }
-            .themeLockScreen()
-            .environmentObject(theme)
-            .environmentObject(context.iapManager)
-            .environmentObject(context.connectionObserver)
-        }
-#if os(macOS)
-        .defaultSize(width: 600.0, height: 400.0)
+#if os(iOS)
+        WindowGroup(content: content)
+#else
+        Window(appName, id: appName, content: content)
+            .defaultSize(width: 600.0, height: 400.0)
 #endif
+    }
+}
+
+private extension PassepartoutApp {
+    func content() -> some View {
+        AppCoordinator(
+            profileManager: context.profileManager,
+            tunnel: context.tunnel,
+            registry: context.registry
+        )
+        .onLoad {
+            CommonLibrary.configureLogging(
+                to: Constants.shared.urlForAppLog,
+                parameters: Constants.shared.log
+            )
+            AppUI.configure(with: context)
+        }
+        .themeLockScreen()
+        .environmentObject(theme)
+        .environmentObject(context.iapManager)
+        .environmentObject(context.connectionObserver)
     }
 }