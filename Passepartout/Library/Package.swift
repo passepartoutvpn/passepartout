27d93d94462458aa05ae3e3fe97ba5924145d7a5
Davide <keeshux@gmail.com>
2024-10-19 16:40:37 +0200

    Fix library for manager updates

--- Passepartout/Library/Package.swift
+++ Passepartout/Library/Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", from: "0.9.0"),
-        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "9a43e23e9134c3e93926271b2d630be607433fa0"),
+        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "377677cab4fd2ab45fccd807a886c3d41d3ff32b"),
 //        .package(path: "../../../passepartoutkit-source"),
         .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", from: "0.9.1"),
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", revision: "031863a1cd683962a7dfe68e20b91fa820a1ecce"),
