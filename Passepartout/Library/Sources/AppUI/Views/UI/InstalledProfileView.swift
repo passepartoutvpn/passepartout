6f64edc95b57c2ffef3ccd104bd9bd98ad8ef64a
Davide <keeshux@gmail.com>
2024-10-28 21:31:48 +0100

    Fix overflow in installed profile name

--- Passepartout/Library/Sources/AppUI/Views/UI/InstalledProfileView.swift
+++ Passepartout/Library/Sources/AppUI/Views/UI/InstalledProfileView.swift
@@ -91,7 +91,6 @@ private extension InstalledProfileView {
 
     var nameView: some View {
         Text(profile?.name ?? Strings.Views.Profiles.Rows.notInstalled)
-            .fixedSize()
             .font(.title2)
             .fontWeight(theme.relevantWeight)
             .themeTruncating(.tail)
