bb8c7602783521bab5d3deb5a5926afdb6baffaf
Davide <keeshux@gmail.com>
2024-10-30 17:07:59 +0100

    OpenVPN OTP is persisted with the password (#785)
    Encode OpenVPN password + OTP in tunnel rather than in the app.
    
    Encoding them upfront in the app ends up persisting the profile with the
    combined password. Update the library with a new OTP field in
    OpenVPN.Credentials, so that the password encoding is performed [on the
    fly in the
    tunnel](https://github.com/passepartoutvpn/passepartoutkit-source/pull/398).
    Similar to how provider modules are generated.
    
    This is likely a regression caused by migrating to NEProfileRepository,
    because starting a connection causes the profile to be saved to NE with
    the encoded password. Later, the profile is restored from NE and
    therefore contains the encoded password.

--- Passepartout/Library/Package.swift
+++ Passepartout/Library/Package.swift
@@ -36,7 +36,7 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", from: "0.9.0"),
-        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "7426954e8ec84e6ddf6c8cd415bfa2c5c7064534"),
+        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "8b4c47f716120fab3f219593cf4ae0e6e2c86677"),
 //        .package(path: "../../../passepartoutkit-source"),
         .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", from: "0.9.1"),
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", revision: "031863a1cd683962a7dfe68e20b91fa820a1ecce"),
