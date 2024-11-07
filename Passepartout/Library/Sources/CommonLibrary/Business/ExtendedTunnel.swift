passepartoutvpn/passepartout

commit 68df6066ba0663c6b240de296c630e38a08c0c6d

Improve configuration on app launch/active (#821)
- Centralize context initialization/refresh in platform-specific app
delegates
- Prevent multiple calls to .onApplicationActive()
- Simplify local/remote profile fingerprint comparison
- Revert to always replacing Core Data entities
- The remote store somehow ended up having duplicates, which caused
repeated imports of remote profiles due to randomly different
fingerprints
- Optimize reload of in-app receipt

Passepartout/Library/Sources/UILibrary/Business/AppContext.swift
@@ -41,6 +41,8 @@ public final class AppContext: ObservableObject {
 
     public let providerManager: ProviderManager
 
+    private var isActivating = false
+
     private var subscriptions: Set<AnyCancellable>
 
     public init(
@@ -61,16 +63,20 @@ public final class AppContext: ObservableObject {
     }
 
     public func onApplicationActive() {
+        guard !isActivating else {
+            return
+        }
+        isActivating = true
         Task {
             do {
                 pp_log(.app, .notice, "Application became active")
-                pp_log(.app, .notice, "Reload IAP receipt...")
                 await iapManager.reloadReceipt()
                 pp_log(.app, .notice, "Prepare tunnel and purge stale data...")
                 try await tunnel.prepare(purge: true)
             } catch {
                 pp_log(.app, .fault, "Unable to prepare tunnel: \(error)")
             }
+            isActivating = false
         }
     }
 }

Passepartout.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
@@ -41,7 +41,7 @@
       "kind" : "remoteSourceControl",
       "location" : "git@github.com:passepartoutvpn/passepartoutkit-source",
       "state" : {
-        "revision" : "caf31aff2e2641356de0d01f3c2c2d0d635d6a2b"
+        "revision" : "e7bd9636ac31d6111b0bc7c171398e68eeb384b5"
       }
     },
     {

Passepartout/App/AppDelegate.swift
@@ -35,6 +35,11 @@ final class AppDelegate: NSObject {
 
     func configure(with uiConfiguring: UILibraryConfiguring) {
         UILibrary(uiConfiguring)
-            .configure(with: context)
+            .configure()
+
+        Task {
+            pp_log(.app, .notice, "Fetch providers index...")
+            try await context.providerManager.fetchIndex(from: API.shared)
+        }
     }
 }

Passepartout/App/Platforms/App+iOS.swift
@@ -30,7 +30,7 @@ import SwiftUI
 
 extension AppDelegate: UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        configure(with: AppUIMain(isStartedFromLoginItem: false))
+        configure(with: AppUIMain())
         return true
     }
 }

Passepartout/App/Platforms/App+macOS.swift
@@ -33,8 +33,11 @@ import SwiftUI
 
 extension AppDelegate: NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
-        configure(with: AppUIMain(isStartedFromLoginItem: isStartedFromLoginItem))
-        hideIfLoginItem()
+        configure(with: AppUIMain())
+        context.onApplicationActive()
+        if isStartedFromLoginItem {
+            AppWindow.shared.isVisible = false
+        }
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -59,12 +62,6 @@ private extension AppDelegate {
 
     var isStartedFromLoginItem: Bool {
         NSApp.isHidden
-    }
-
-    func hideIfLoginItem() {
-        if isStartedFromLoginItem {
-            AppWindow.shared.isVisible = false
-        }
     }
 }
 

Passepartout/Library/Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", from: "0.9.0"),
-        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "caf31aff2e2641356de0d01f3c2c2d0d635d6a2b"),
+        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "e7bd9636ac31d6111b0bc7c171398e68eeb384b5"),
 //        .package(path: "../../../passepartoutkit-source"),
         .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", from: "0.9.1"),
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", revision: "031863a1cd683962a7dfe68e20b91fa820a1ecce"),

Passepartout/Library/Sources/AppDataProfiles/CDProfileRepositoryV3.swift
@@ -50,7 +50,7 @@ extension AppData {
         } fromMapper: {
             try fromMapper($0, registry: registry, coder: coder)
         } toMapper: {
-            try toMapper($0, $1, $2, registry: registry, coder: coder)
+            try toMapper($0, $1, registry: registry, coder: coder)
         } onResultError: {
             onResultError?($0) ?? .ignore
         }
@@ -73,14 +73,13 @@ private extension AppData {
 
     static func toMapper(
         _ profile: Profile,
-        _ oldCdEntity: CDProfileV3?,
         _ context: NSManagedObjectContext,
         registry: Registry,
         coder: ProfileCoder
     ) throws -> CDProfileV3 {
         let encoded = try registry.encodedProfile(profile, with: coder)
 
-        let cdProfile = oldCdEntity ?? CDProfileV3(context: context)
+        let cdProfile = CDProfileV3(context: context)
         cdProfile.uuid = profile.id
         cdProfile.name = profile.name
         cdProfile.encoded = encoded

Passepartout/Library/Sources/AppUIMain/AppUIMain.swift
@@ -27,15 +27,11 @@ import Foundation
 @_exported import UILibrary
 
 public final class AppUIMain: UILibraryConfiguring {
-    private let isStartedFromLoginItem: Bool
-
-    public init(isStartedFromLoginItem: Bool) {
-        self.isStartedFromLoginItem = isStartedFromLoginItem
+    public init() {
     }
 
-    public func configure(with context: AppContext) {
+    public func configure() {
         assertMissingImplementations()
-        context.onApplicationActive()
     }
 }
 

commit abe4c779b8d95b99b7e7332abe92b7eda0b0a97f

Improve startup time (#822)
Profiles were loaded after reading receipt, which took at least a second
on iOS.

Potential regression from #821

Passepartout/Library/Sources/CommonLibrary/Business/ExtendedTunnel.swift
@@ -99,20 +99,24 @@ extension ExtendedTunnel {
     }
 
     public func prepare(purge: Bool) async throws {
+        pp_log(.app, .notice, "Prepare tunnel and purge stale data (\(purge))...")
         try await tunnel.prepare(purge: purge)
     }
 
     public func install(_ profile: Profile) async throws {
+        pp_log(.app, .notice, "Install profile \(profile.id)...")
         let newProfile = try processedProfile(profile)
         try await tunnel.install(newProfile, connect: false, title: processedTitle)
     }
 
     public func connect(with profile: Profile) async throws {
+        pp_log(.app, .notice, "Connect to profile \(profile.id)...")
         let newProfile = try processedProfile(profile)
         try await tunnel.install(newProfile, connect: true, title: processedTitle)
     }
 
     public func disconnect() async throws {
+        pp_log(.app, .notice, "Disconnect...")
         try await tunnel.disconnect()
     }
 

Passepartout/Library/Sources/UILibrary/Business/AppContext.swift
@@ -67,14 +67,19 @@ public final class AppContext: ObservableObject {
             return
         }
         isActivating = true
+        pp_log(.app, .notice, "Application became active")
         Task {
-            do {
-                pp_log(.app, .notice, "Application became active")
-                await iapManager.reloadReceipt()
-                pp_log(.app, .notice, "Prepare tunnel and purge stale data...")
-                try await tunnel.prepare(purge: true)
-            } catch {
-                pp_log(.app, .fault, "Unable to prepare tunnel: \(error)")
+            await withTaskGroup(of: Void.self) { group in
+                group.addTask {
+                    do {
+                        try await self.tunnel.prepare(purge: true)
+                    } catch {
+                        pp_log(.app, .fault, "Unable to prepare tunnel: \(error)")
+                    }
+                }
+                group.addTask {
+                    await self.iapManager.reloadReceipt()
+                }
             }
             isActivating = false
         }