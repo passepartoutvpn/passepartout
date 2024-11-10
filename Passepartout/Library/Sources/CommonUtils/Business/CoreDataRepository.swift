passepartoutvpn/passepartout

commit 21340e9f564e439e7588bbb9a709068b063f555b

Rewrite AppContext event handlers (#839)
Loading remote profiles before local profiles may cause duplicated NE
managers. This happened because if local profiles are empty, any remote
profile is imported regardless of their former existence in the local
store. The importer just doesn't know.

Therefore, revisit the sequence of AppContext registrations:

- First off
- Skip Tunnel prepare() because NEProfileRepository.fetch() does it
already
- NE is both Tunnel and ProfileRepository, so calling tunnel.prepare()
loads local NE profiles twice
- onLaunch() - **run this once and before anything else**
  - Read local profiles
  - Reload in-app receipt
  - Observe in-app eligibility → Triggers onEligibleFeatures()
  - Observe profile save → Triggers onSaveProfile()
  - Fetch providers index
- onForeground()
  - Read local profiles
  - Read remote profiles, and toggle CloudKit sync based on eligibility
- onEligibleFeatures()
  - Read remote profiles, and toggle CloudKit sync based on eligibility
- onSaveProfile()
  - Reconnect if necessary

Passepartout/Library/Sources/CommonUtils/Business/CoreDataRepository.swift
@@ -89,16 +89,6 @@ public actor CoreDataRepository<CD, T>: NSObject,
             sectionNameKeyPath: nil,
             cacheName: nil
         )
-
-        super.init()
-
-        resultsController.delegate = self
-        do {
-            try resultsController.performFetch()
-            sendResults(from: resultsController)
-        } catch {
-            //
-        }
     }
 
     public nonisolated var entitiesPublisher: AnyPublisher<EntitiesResult<T>, Never> {
@@ -111,6 +101,10 @@ public actor CoreDataRepository<CD, T>: NSObject,
     }
 
     public func resetFilter() async throws {
+        try await filter(byPredicate: nil)
+    }
+
+    public func fetchAllEntities() async throws -> [T] {
         try await filter(byPredicate: nil)
     }
 
@@ -154,7 +148,6 @@ public actor CoreDataRepository<CD, T>: NSObject,
             do {
                 let existing = try context.fetch(request)
                 existing.forEach(context.delete)
-
                 try context.save()
             } catch {
                 context.rollback()
@@ -173,7 +166,9 @@ public actor CoreDataRepository<CD, T>: NSObject,
         guard let cdController = controller as? NSFetchedResultsController<CD> else {
             fatalError("Unable to upcast results to \(CD.self)")
         }
-        sendResults(from: cdController)
+        Task.detached { [weak self] in
+            await self?.sendResults(from: cdController)
+        }
     }
 }
 
@@ -182,7 +177,12 @@ private extension CoreDataRepository {
         case mapping(Error)
     }
 
-    func filter(byPredicate predicate: NSPredicate?) async throws {
+    nonisolated func newFetchRequest() -> NSFetchRequest<CD> {
+        NSFetchRequest(entityName: entityName)
+    }
+
+    @discardableResult
+    func filter(byPredicate predicate: NSPredicate?) async throws -> [T] {
         let request = resultsController.fetchRequest
         request.predicate = predicate
         resultsController = NSFetchedResultsController(
@@ -193,61 +193,73 @@ private extension CoreDataRepository {
         )
         resultsController.delegate = self
         try resultsController.performFetch()
-        sendResults(from: resultsController)
+        return await sendResults(from: resultsController)
     }
 
-    nonisolated func newFetchRequest() -> NSFetchRequest<CD> {
-        NSFetchRequest(entityName: entityName)
+    @discardableResult
+    func sendResults(from controller: NSFetchedResultsController<CD>) async -> [T] {
+        await context.perform {
+            self.unsafeSendResults(from: controller)
+        }
     }
 
-    nonisolated func sendResults(from controller: NSFetchedResultsController<CD>) {
-        Task.detached { [weak self] in
-            await self?.context.perform { [weak self] in
-                guard let cdEntities = controller.fetchedObjects else {
-                    return
-                }
+    @discardableResult
+    func unsafeSendResults(from controller: NSFetchedResultsController<CD>) -> [T] {
+        guard let cdEntities = controller.fetchedObjects else {
+            return []
+        }
 
-                // strip duplicates by sort order (first entry wins)
-                var knownUUIDs = Set<UUID>()
-                cdEntities.forEach {
-                    guard let uuid = $0.uuid else {
-                        return
-                    }
-                    guard !knownUUIDs.contains(uuid) else {
-                        NSLog("Strip duplicate \(String(describing: CD.self)) with UUID \(uuid)")
-                        self?.context.delete($0)
-                        return
-                    }
-                    knownUUIDs.insert(uuid)
-                }
+        var entitiesToDelete: [CD] = []
 
+        // strip duplicates by sort order (first entry wins)
+        var knownUUIDs = Set<UUID>()
+        cdEntities.forEach {
+            guard let uuid = $0.uuid else {
+                return
+            }
+            guard !knownUUIDs.contains(uuid) else {
+                NSLog("Strip duplicate \(String(describing: CD.self)) with UUID \(uuid)")
+                entitiesToDelete.append($0)
+                return
+            }
+            knownUUIDs.insert(uuid)
+        }
+
+        do {
+            let entities = try cdEntities.compactMap {
                 do {
-                    let entities = try cdEntities.compactMap {
-                        do {
-                            return try self?.fromMapper($0)
-                        } catch {
-                            switch self?.onResultError?(error) {
-                            case .discard:
-                                self?.context.delete($0)
-
-                            case .halt:
-                                throw ResultError.mapping(error)
-
-                            default:
-                                break
-                            }
-                            return nil
-                        }
-                    }
-
-                    try self?.context.save()
-
-                    let result = EntitiesResult(entities, isFiltering: controller.fetchRequest.predicate != nil)
-                    self?.entitiesSubject.send(result)
+                    return try fromMapper($0)
                 } catch {
-                    NSLog("Unable to send Core Data entities: \(error)")
+                    switch onResultError?(error) {
+                    case .discard:
+                        entitiesToDelete.append($0)
+
+                    case .halt:
+                        throw ResultError.mapping(error)
+
+                    default:
+                        break
+                    }
+                    return nil
                 }
             }
+
+            if !entitiesToDelete.isEmpty {
+                do {
+                    entitiesToDelete.forEach(context.delete)
+                    try context.save()
+                } catch {
+                    NSLog("Unable to delete Core Data entities: \(error)")
+                    context.rollback()
+                }
+            }
+
+            let result = EntitiesResult(entities, isFiltering: controller.fetchRequest.predicate != nil)
+            entitiesSubject.send(result)
+            return result.entities
+        } catch {
+            NSLog("Unable to send Core Data entities: \(error)")
+            return []
         }
     }
 }

Passepartout.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
@@ -41,7 +41,7 @@
       "kind" : "remoteSourceControl",
       "location" : "git@github.com:passepartoutvpn/passepartoutkit-source",
       "state" : {
-        "revision" : "b31816d060e40583a27d22ea5c59cc686c057aaf"
+        "revision" : "3a4c78af67dfe181acc657a5539ee3d62d1c9361"
       }
     },
     {

Passepartout/App/AppDelegate.swift
@@ -36,10 +36,5 @@ final class AppDelegate: NSObject {
     func configure(with uiConfiguring: UILibraryConfiguring) {
         UILibrary(uiConfiguring)
             .configure(with: context)
-
-        Task {
-            pp_log(.app, .notice, "Fetch providers index...")
-            try await context.providerManager.fetchIndex(from: API.shared)
         }
-    }
 }

Passepartout/Library/Package.swift
@@ -46,7 +46,7 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", from: "0.9.0"),
-        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "b31816d060e40583a27d22ea5c59cc686c057aaf"),
+        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source", revision: "3a4c78af67dfe181acc657a5539ee3d62d1c9361"),
 //        .package(path: "../../../passepartoutkit-source"),
         .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", from: "0.9.1"),
 //        .package(url: "git@github.com:passepartoutvpn/passepartoutkit-source-openvpn-openssl", revision: "031863a1cd683962a7dfe68e20b91fa820a1ecce"),

Passepartout/Library/Sources/AppDataProfiles/CDProfileRepositoryV3.swift
@@ -41,19 +41,23 @@ extension AppData {
     ) -> ProfileRepository {
         let repository = CoreDataRepository<CDProfileV3, Profile>(
             context: context,
-            observingResults: observingResults
-        ) {
-            $0.sortDescriptors = [
-                .init(key: "name", ascending: true, selector: #selector(NSString.caseInsensitiveCompare)),
-                .init(key: "lastUpdate", ascending: false)
-            ]
-        } fromMapper: {
-            try fromMapper($0, registry: registry, coder: coder)
-        } toMapper: {
-            try toMapper($0, $1, registry: registry, coder: coder)
-        } onResultError: {
-            onResultError?($0) ?? .ignore
-        }
+            observingResults: observingResults,
+            beforeFetch: {
+                $0.sortDescriptors = [
+                    .init(key: "name", ascending: true, selector: #selector(NSString.caseInsensitiveCompare)),
+                    .init(key: "lastUpdate", ascending: false)
+                ]
+            },
+            fromMapper: {
+                try fromMapper($0, registry: registry, coder: coder)
+            },
+            toMapper: {
+                try toMapper($0, $1, registry: registry, coder: coder)
+            },
+            onResultError: {
+                onResultError?($0) ?? .ignore
+            }
+        )
         return repository
     }
 }
@@ -110,6 +114,10 @@ extension CoreDataRepository: ProfileRepository where T == Profile {
         entitiesPublisher
             .map(\.entities)
             .eraseToAnyPublisher()
+    }
+
+    public func fetchProfiles() async throws -> [Profile] {
+        try await fetchAllEntities()
     }
 
     public func saveProfile(_ profile: Profile) async throws {

Passepartout/Library/Sources/CommonLibrary/Business/ExtendedTunnel.swift
@@ -98,11 +98,6 @@ extension ExtendedTunnel {
         tunnel.currentProfile
     }
 
-    public func prepare(purge: Bool) async throws {
-        pp_log(.app, .notice, "Prepare tunnel and purge stale data (\(purge))...")
-        try await tunnel.prepare(purge: purge)
-    }
-
     public func install(_ profile: Profile) async throws {
         pp_log(.app, .notice, "Install profile \(profile.id)...")
         let newProfile = try processedProfile(profile)

Passepartout/Library/Sources/CommonLibrary/Business/InMemoryProfileRepository.swift
@@ -45,6 +45,10 @@ public final class InMemoryProfileRepository: ProfileRepository {
         profilesSubject.eraseToAnyPublisher()
     }
 
+    public func fetchProfiles() async throws -> [Profile] {
+        profiles
+    }
+
     public func saveProfile(_ profile: Profile) {
         pp_log(.App.profiles, .info, "Save profile: \(profile.id))")
         if let index = profiles.firstIndex(where: { $0.id == profile.id }) {

Passepartout/Library/Sources/CommonLibrary/Business/NEProfileRepository.swift
@@ -45,14 +45,6 @@ public final class NEProfileRepository: ProfileRepository {
 
         repository
             .managersPublisher
-            .first()
-            .sink { [weak self] in
-                self?.onLoadedManagers($0)
-            }
-            .store(in: &subscriptions)
-
-        repository
-            .managersPublisher
             .dropFirst()
             .sink { [weak self] in
                 self?.onUpdatedManagers($0)
@@ -62,6 +54,20 @@ public final class NEProfileRepository: ProfileRepository {
 
     public var profilesPublisher: AnyPublisher<[Profile], Never> {
         profilesSubject.eraseToAnyPublisher()
+    }
+
+    public func fetchProfiles() async throws -> [Profile] {
+        let managers = try await repository.fetch()
+        let profiles = managers.compactMap {
+            do {
+                return try repository.profile(from: $0)
+            } catch {
+                pp_log(.App.profiles, .error, "Unable to decode profile from NE manager '\($0.localizedDescription ?? "")': \(error)")
+                return nil
+            }
+        }
+        profilesSubject.send(profiles)
+        return profiles
     }
 
     public func saveProfile(_ profile: Profile) async throws {
@@ -74,6 +80,9 @@ public final class NEProfileRepository: ProfileRepository {
     }
 
     public func removeProfiles(withIds profileIds: [Profile.ID]) async throws {
+        guard !profileIds.isEmpty else {
+            return
+        }
         var removedIds: Set<Profile.ID> = []
         defer {
             profilesSubject.value.removeAll {
@@ -92,18 +101,6 @@ public final class NEProfileRepository: ProfileRepository {
 }
 
 private extension NEProfileRepository {
-    func onLoadedManagers(_ managers: [Profile.ID: NETunnelProviderManager]) {
-        let profiles = managers.values.compactMap {
-            do {
-                return try repository.profile(from: $0)
-            } catch {
-                pp_log(.App.profiles, .error, "Unable to decode profile from NE manager '\($0.localizedDescription ?? "")': \(error)")
-                return nil
-            }
-        }
-        profilesSubject.send(profiles)
-    }
-
     func onUpdatedManagers(_ managers: [Profile.ID: NETunnelProviderManager]) {
         let profiles = profilesSubject
             .value

commit a8022edfa72a321a21ef44961cfd570ddda4d6aa

Fix regression about handling of Core Data duplicates
Regression in #839 due to how NSFetchedResultsController was
refactored. Duplicated entities were not excluded from mapping.

Passepartout/Library/Sources/CommonUtils/Business/CoreDataRepository.swift
@@ -205,7 +205,7 @@ private extension CoreDataRepository {
 
     @discardableResult
     func unsafeSendResults(from controller: NSFetchedResultsController<CD>) -> [T] {
-        guard let cdEntities = controller.fetchedObjects else {
+        guard var cdEntities = controller.fetchedObjects else {
             return []
         }
 
@@ -226,6 +226,9 @@ private extension CoreDataRepository {
         }
 
         do {
+            cdEntities.removeAll {
+                entitiesToDelete.contains($0)
+            }
             let entities = try cdEntities.compactMap {
                 do {
                     return try fromMapper($0)
@@ -243,7 +246,6 @@ private extension CoreDataRepository {
                     return nil
                 }
             }
-
             if !entitiesToDelete.isEmpty {
                 do {
                     entitiesToDelete.forEach(context.delete)