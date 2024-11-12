3ed779816f4666f40d6dabd89544078470d7d2b9
Davide <keeshux@gmail.com>
2024-11-12 11:08:37 +0100

    Test two profile migrations

--- Passepartout/Library/Tests/LegacyV2Tests/LegacyV2Tests.swift
+++ Passepartout/Library/Tests/LegacyV2Tests/LegacyV2Tests.swift
@@ -65,7 +65,43 @@ final class LegacyV2Tests: XCTestCase {
         XCTAssertEqual(Set(migratable.map(\.name)), Set(expectedNames))
     }
 
-    func test_givenStore_whenFetch_thenReturnsProfilesV3() async throws {
+    func test_givenStore_whenMigrateHideMe_thenIsExpected() async throws {
+        let sut = newStore()
+
+        let id = try XCTUnwrap(UUID(uuidString: "8A568345-85C4-44C1-A9C4-612E8B07ADC5"))
+        let migrated = try await sut.fetchProfiles(selection: [id])
+
+        XCTAssertEqual(migrated.count, 1)
+        let profile = try XCTUnwrap(migrated.first)
+        XCTAssertEqual(profile.id, id)
+        XCTAssertEqual(profile.name, "Hide.me")
+        XCTAssertEqual(profile.attributes.lastUpdate, Date(timeIntervalSinceReferenceDate: 673117681.24825))
+
+        XCTAssertEqual(profile.modules.count, 3)
+
+        let onDemand = try XCTUnwrap(profile.firstModule(ofType: OnDemandModule.self))
+        XCTAssertTrue(onDemand.isEnabled)
+        XCTAssertEqual(onDemand.policy, .excluding)
+        XCTAssertEqual(onDemand.withSSIDs, [
+            "Safe Wi-Fi": true,
+            "Friend's House": false
+        ])
+        XCTAssertTrue(onDemand.withOtherNetworks.isEmpty)
+
+        let openVPN = try XCTUnwrap(profile.firstModule(ofType: OpenVPNModule.self))
+        XCTAssertEqual(openVPN.credentials?.username, "foo")
+        XCTAssertEqual(openVPN.credentials?.password, "bar")
+
+        let dns = try XCTUnwrap(profile.firstModule(ofType: DNSModule.self))
+        let dohURL = try XCTUnwrap(URL(string: "https://1.1.1.1/dns-query"))
+        XCTAssertEqual(dns.protocolType, .https(url: dohURL))
+        XCTAssertEqual(dns.servers, [
+            Address(rawValue: "1.1.1.1"),
+            Address(rawValue: "1.0.0.1")
+        ])
+    }
+
+    func test_givenStore_whenMigrateProtonVPN_thenIsExpected() async throws {
         let sut = newStore()
 
         let id = try XCTUnwrap(UUID(uuidString: "981E7CBD-7733-4CF3-9A51-2777614ED5D4"))
@@ -78,8 +114,16 @@ final class LegacyV2Tests: XCTestCase {
         XCTAssertEqual(profile.attributes.lastUpdate, Date(timeIntervalSinceReferenceDate: 724509584.854822))
 
         XCTAssertEqual(profile.modules.count, 2)
+
         let onDemand = try XCTUnwrap(profile.firstModule(ofType: OnDemandModule.self))
+        XCTAssertTrue(onDemand.isEnabled)
+        XCTAssertEqual(onDemand.policy, .excluding)
+        XCTAssertTrue(onDemand.withSSIDs.isEmpty)
+        XCTAssertTrue(onDemand.withOtherNetworks.isEmpty)
+
         let openVPN = try XCTUnwrap(profile.firstModule(ofType: OpenVPNModule.self))
+        XCTAssertEqual(openVPN.credentials?.username, "foo")
+        XCTAssertEqual(openVPN.credentials?.password, "bar")
     }
 }
 
