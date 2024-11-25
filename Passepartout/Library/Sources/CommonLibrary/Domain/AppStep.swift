abc3142a1fe23a049a8ce2074ef4a4b374a96d7a
Davide <keeshux@gmail.com>
2024-11-25 21:17:03 +0100

    Add and test AppStep

---
+++ Passepartout/Library/Sources/CommonLibrary/Domain/AppStep.swift
@@ -0,0 +1,50 @@
+//
+//  AppStep.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 11/25/24.
+//  Copyright (c) 2024 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+public enum AppStep: String, RawRepresentable, CaseIterable {
+    case migrateV3
+
+    case community
+
+    case doneV3
+}
+
+extension Optional where Wrapped == AppStep {
+    public var nextStep: AppStep? {
+        let all = AppStep.allCases
+        guard let self else {
+            return all.first
+        }
+        guard let index = all.firstIndex(of: self) else {
+            fatalError("How can self not be part of allCases?")
+        }
+        guard index < all.count - 1 else {
+            return self
+        }
+        return all[index + 1]
+    }
+}
