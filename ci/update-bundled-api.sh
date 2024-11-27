fc711ae98fc9e7f0a4c471254eb030c621e55605
Davide <keeshux@gmail.com>
2024-11-27 19:27:13 +0100

    Get rid of downloaded API after update

--- ci/update-bundled-api.sh
+++ ci/update-bundled-api.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
+SOURCE=".api"
 DESTINATION="Passepartout/Library/Sources/CommonAPI/API"
 API_VERSION="v5"
 
-mkdir tmp
-cd tmp
-if [[ ! `git clone https://github.com/passepartoutvpn/api --depth 1` ]]; then
-    cd api
-    git pull
-fi
-cd ../..
+rm -rf $SOURCE
+git clone https://github.com/passepartoutvpn/api --depth 1 $SOURCE
 
 rm -rf $DESTINATION
 mkdir -p $DESTINATION
-cp -rp tmp/api/$API_VERSION $DESTINATION
+mv $SOURCE/$API_VERSION $DESTINATION
+rm -rf $SOURCE
