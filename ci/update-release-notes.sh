752a1b19931fe53c34eaf0b18c5f1e418e8875c2
Davide <keeshux@gmail.com>
2024-12-11 21:40:08 +0100

    Fix release_notes destination

--- ci/update-release-notes.sh
+++ ci/update-release-notes.sh
@@ -3,7 +3,7 @@ CHANGELOG="CHANGELOG.txt"
 PLATFORMS="iOS macOS tvOS"
 
 for PLATFORM in $PLATFORMS; do
-    DST="fastlane/metadata/$PLATFORM/en-US/release_notes.txt"
+    DST="fastlane/metadata/$PLATFORM/default/release_notes.txt"
     rm -f "$DST"
     cp "$CHANGELOG" "$DST"
 done
