a5c5d7589de9909d831748ff4cca3bb88fed8a9f
Davide <keeshux@gmail.com>
2024-12-11 19:59:58 +0100

    [ci skip] Fix tabs

--- screenshots/export.sh
+++ screenshots/export.sh
@@ -13,22 +13,22 @@ case $device in
 
   "iphone")
     xcplan="MainScreenshots"
-	xcdestination="name=iPhone 16 Pro Max"
+    xcdestination="name=iPhone 16 Pro Max"
     ;;
 
   "ipad")
     xcplan="MainScreenshots"
-	xcdestination="name=iPad (10th generation)"
+    xcdestination="name=iPad (10th generation)"
     ;;
 
   "mac")
     xcplan="MainScreenshots"
-	xcdestination="platform=macOS,arch=arm64"
+    xcdestination="platform=macOS,arch=arm64"
     ;;
 
   "appletv")
     xcplan="TVScreenshots"
-	xcdestination="name=Apple TV 4K (3rd generation)"
+    xcdestination="name=Apple TV 4K (3rd generation)"
     ;;
 
   *)
@@ -47,8 +47,8 @@ xcparse screenshots "$results_path" "$screenshots_path"
 # 3. drop the filename suffix
 cd "$screenshots_path"
 for file in 0[1-9]_*.png; do
-	if [[ -e "$file" ]]; then
-		new_name="${file%%_*}.png"
-		mv "$file" "$new_name"
-	fi
+    if [[ -e "$file" ]]; then
+        new_name="${file%%_*}.png"
+        mv "$file" "$new_name"
+    fi
 done
