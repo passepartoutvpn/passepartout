67841a35fc00460728c359a5498822fc1cbaa7cd
Davide <keeshux@gmail.com>
2024-12-17 12:45:21 +0100

    Decompose vars

--- scripts/copy-release-notes.sh
+++ scripts/copy-release-notes.sh
@@ -6,8 +6,9 @@ if [[ -n "$1" ]]; then
 fi
 changelog="$cwd/../CHANGELOG.txt"
 metadata_root="$cwd/../fastlane/metadata"
+metadata_path="default/release_notes.txt"
 for platform in $platforms; do
-    release_notes="$metadata_root/$platform/default/release_notes.txt"
+    release_notes="$metadata_root/$platform/$metadata_path"
     rm -f "$release_notes"
     cp "$changelog" "$release_notes"
 done
