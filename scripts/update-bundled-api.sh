a548add83b470a5cbb58da2755b04507f5cbbb71
Davide <keeshux@gmail.com>
2024-12-17 12:47:18 +0100

    Prefix with api_

--- scripts/update-bundled-api.sh
+++ scripts/update-bundled-api.sh
@@ -2,12 +2,12 @@
 cwd=`dirname $0`
 api_version="v5"
 api_path="$cwd/../.api"
-package_path="$cwd/../Library/Sources/CommonAPI/API"
+api_package_path="$cwd/../Library/Sources/CommonAPI/API"
 
 rm -rf $api_path
 git clone https://github.com/passepartoutvpn/api --depth 1 $api_path
 
-rm -rf $package_path
-mkdir -p $package_path
-mv $api_path/$api_version $package_path
+rm -rf $api_package_path
+mkdir -p $api_package_path
+mv $api_path/$api_version $api_package_path
 rm -rf $api_path
