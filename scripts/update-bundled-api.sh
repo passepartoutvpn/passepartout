6c663e1e8cac0001781d059b761dd40e68022d82
Davide <keeshux@gmail.com>
2024-12-17 12:22:43 +0100

    Make API script relative

--- scripts/update-bundled-api.sh
+++ scripts/update-bundled-api.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-SOURCE=".api"
-DESTINATION="Library/Sources/CommonAPI/API"
-API_VERSION="v5"
+cwd=`dirname $0`
+api_version="v5"
+api_path="$cwd/../.api"
+package_path="$cwd/../Library/Sources/CommonAPI/API"
 
-rm -rf $SOURCE
-git clone https://github.com/passepartoutvpn/api --depth 1 $SOURCE
+rm -rf $api_path
+git clone https://github.com/passepartoutvpn/api --depth 1 $api_path
 
-rm -rf $DESTINATION
-mkdir -p $DESTINATION
-mv $SOURCE/$API_VERSION $DESTINATION
-rm -rf $SOURCE
+rm -rf $package_path
+mkdir -p $package_path
+mv $api_path/$api_version $package_path
+rm -rf $api_path
