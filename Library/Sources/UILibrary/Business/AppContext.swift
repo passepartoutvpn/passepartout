passepartoutvpn/passepartout

commit 6255a273feb159e0d6e3b61375e1262ce3b2a6c8

Add recurring products to paywall (#1019)
Suggest whenever .fullTV is suggested. For this reason, subscriptions
are not suggested to existing full version purchasers, because they only
miss the one-time .appleTV purchase. Group purchases by feature products
and full version products, e.g.:

- Features
  - Apple TV
- Full version
  - Yearly
  - Monthly
  - Full
  - Full + TV

Additionally, fix reloadReceipt() slowing down onLaunch() sometimes. The
warning sign would appear on restricted profiles until the receipt is
loaded.

Library/Sources/UILibrary/Business/AppContext.swift
@@ -98,7 +98,9 @@ private extension AppContext {
 
         pp_log(.App.profiles, .info, "\tObserve in-app events...")
         iapManager.observeObjects()
-        await iapManager.reloadReceipt()
+        Task {
+            await iapManager.reloadReceipt()
+        }
 
         pp_log(.App.profiles, .info, "\tObserve eligible features...")
         iapManager

Library/.swiftpm/xcode/xcshareddata/xcschemes/Library-Package.xcscheme
@@ -147,6 +147,20 @@
                ReferencedContainer = "container:">
             </BuildableReference>
          </BuildActionEntry>
+         <BuildActionEntry
+            buildForTesting = "YES"
+            buildForRunning = "YES"
+            buildForProfiling = "YES"
+            buildForArchiving = "YES"
+            buildForAnalyzing = "YES">
+            <BuildableReference
+               BuildableIdentifier = "primary"
+               BlueprintIdentifier = "UIAccessibility"
+               BuildableName = "UIAccessibility"
+               BlueprintName = "UIAccessibility"
+               ReferencedContainer = "container:">
+            </BuildableReference>
+         </BuildActionEntry>
       </BuildActionEntries>
    </BuildAction>
    <TestAction

Library/Sources/CommonIAP/Domain/AppProduct+Features.swift
@@ -81,6 +81,26 @@ extension AppProduct.Full {
     }
 }
 
+extension AppProduct {
+    public var isFullVersion: Bool {
+        switch self {
+        case .Full.OneTime.full, .Full.OneTime.fullTV, .Full.Recurring.monthly, .Full.Recurring.yearly:
+            return true
+        default:
+            return false
+        }
+    }
+
+    public var isRecurring: Bool {
+        switch self {
+        case .Full.Recurring.monthly, .Full.Recurring.yearly:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 // MARK: - Discontinued
 
 extension AppProduct.Features {

Library/Sources/CommonLibrary/IAP/IAPManager+Suggestions.swift
@@ -33,37 +33,42 @@ extension IAPManager {
         purchasedProducts.contains(.Full.OneTime.full) || purchasedProducts.contains(.Full.OneTime.fullTV) || (purchasedProducts.contains(.Full.OneTime.iOS) && purchasedProducts.contains(.Full.OneTime.macOS))
     }
 
-    public func suggestedProducts(for features: Set<AppFeature>) -> [AppProduct] {
-        guard !features.isEmpty else {
-            return []
+    public func suggestedProducts(for requiredFeatures: Set<AppFeature>, withRecurring: Bool = true) -> Set<AppProduct>? {
+        guard !requiredFeatures.isEmpty else {
+            return nil
         }
-        guard !eligibleFeatures.isSuperset(of: features) else {
-            return []
+        guard !eligibleFeatures.isSuperset(of: requiredFeatures) else {
+            return nil
         }
 
-        var list: [AppProduct] = []
-        let requiredFeatures = features.subtracting(eligibleFeatures)
+        var products: Set<AppProduct> = []
+        let ineligibleFeatures = requiredFeatures.subtracting(eligibleFeatures)
 
         if isFullVersionPurchaser {
-            if requiredFeatures == [.appleTV] {
-                list.append(.Features.appleTV)
+            if ineligibleFeatures == [.appleTV] {
+                products.insert(.Features.appleTV)
             } else {
                 assertionFailure("Full version purchaser requiring other than [.appleTV]")
             }
         } else { // !isFullVersionPurchaser
-            if requiredFeatures == [.appleTV] {
-                list.append(.Features.appleTV)
-                list.append(.Full.OneTime.fullTV)
-            } else if requiredFeatures.contains(.appleTV) {
-                list.append(.Full.OneTime.fullTV)
+            if ineligibleFeatures == [.appleTV] {
+                products.insert(.Features.appleTV)
+                products.insert(.Full.OneTime.fullTV)
+            } else if ineligibleFeatures.contains(.appleTV) {
+                products.insert(.Full.OneTime.fullTV)
             } else {
-                list.append(.Full.OneTime.full)
                 if !eligibleFeatures.contains(.appleTV) {
-                    list.append(.Full.OneTime.fullTV)
+                    products.insert(.Full.OneTime.fullTV)
                 }
+                products.insert(.Full.OneTime.full)
             }
         }
 
-        return list
+        if withRecurring && products.contains(.Full.OneTime.fullTV) {
+            products.insert(.Full.Recurring.monthly)
+            products.insert(.Full.Recurring.yearly)
+        }
+
+        return products
     }
 }

Library/Sources/LegacyV2/Strategy/CDProfileRepositoryV2.swift
@@ -44,10 +44,7 @@ final class CDProfileRepositoryV2: Sendable {
 
     var hasMigratableProfiles: Bool {
         do {
-            return try context.performAndWait { [weak self] in
-                guard let self else {
-                    return false
-                }
+            return try context.performAndWait {
                 let entities = try CDProfile.fetchRequest().execute()
                 return !entities.compactMap {
                     ($0.encryptedJSON ?? $0.json) != nil

Library/Sources/UILibrary/L10n/SwiftGen+Strings.swift
@@ -763,10 +763,6 @@ public enum Strings {
           /// Restore
           public static let header = Strings.tr("Localizable", "views.paywall.sections.restore.header", fallback: "Restore")
         }
-        public enum SuggestedProduct {
-          /// One-time purchase
-          public static let header = Strings.tr("Localizable", "views.paywall.sections.suggested_product.header", fallback: "One-time purchase")
-        }
       }
     }
     public enum Preferences {

Library/Sources/UILibrary/Resources/en.lproj/Localizable.strings
@@ -78,7 +78,6 @@
 "views.migration.alerts.delete.message" = "Do you want to discard these profiles? You will not be able to recover them later.\n\n%@";
 
 "views.paywall.sections.required_features.header" = "Required features";
-"views.paywall.sections.suggested_product.header" = "One-time purchase";
 "views.paywall.sections.full_products.header" = "Full version";
 "views.paywall.sections.all_features.header" = "Full version includes";
 "views.paywall.sections.restore.header" = "Restore";

Library/Sources/UILibrary/Views/Paywall/PaywallProductViewStyle.swift
@@ -26,9 +26,7 @@
 import Foundation
 
 public enum PaywallProductViewStyle {
-    case oneTime
-
-    case recurring
-
     case donation
+
+    case paywall
 }

commit 75de2029ff227f550c933f616fcbe2d10e5e7378

Always refresh app features onForeground() (#1022)
Even if they haven't changed since the app was last sent to the
background.

Regression from #1019 where the initial .reloadReceipt() call was
wrapped in a Task to make it asynchronous.

Library/Sources/UILibrary/Business/AppContext.swift
@@ -98,14 +98,23 @@ private extension AppContext {
 
         pp_log(.App.profiles, .info, "\tObserve in-app events...")
         iapManager.observeObjects()
+
+        // load in background, see comment right below
         Task {
             await iapManager.reloadReceipt()
         }
 
+        // using Task above (#1019) causes the receipt to be loaded asynchronously.
+        // the initial call to onEligibleFeatures() may execute before the receipt is
+        // loaded and therefore do nothing. with .removeDuplicates(), there would
+        // not be a second chance to call onEligibleFeatures() if the eligible
+        // features haven't changed after reloading the receipt (this is the case
+        // for TestFlight where some features are set statically). that's why it's
+        // commented now
         pp_log(.App.profiles, .info, "\tObserve eligible features...")
         iapManager
             .$eligibleFeatures
-            .removeDuplicates()
+//            .removeDuplicates()
             .sink { [weak self] eligible in
                 Task {
                     try await self?.onEligibleFeatures(eligible)