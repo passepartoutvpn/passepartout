b0170edb4c06ad50ac2c893aed3ef94a4a9659a9
Davide <keeshux@gmail.com>
2024-12-22 10:01:16 +0100

    Merge

--- scripts/import-translations.sh
+++ scripts/import-translations.sh
@@ -35,3 +35,21 @@ BEGIN {
 ' "$1"
 
 echo "Files have been created in the '$translations_input_path' directory."
+
+for lang in `ls $translations_input_path`; do
+    input_path="$translations_input_path/$lang"
+    output_path="$translations_output_path/$lang.lproj/Localizable.strings"
+    keys_path="$output_path.keys"
+    tmp_path="$output_path.tmp"
+
+    # remove keys
+    sed -E 's/^"(.*)" = .*$/\1/' $input_path >$keys_path
+    grep -vf $keys_path $output_path >$tmp_path
+
+    # append new strings
+    cat $input_path >>$tmp_path
+
+    # sort and replace
+    sort $tmp_path >$output_path
+    rm "$keys_path" "$tmp_path"
+done
