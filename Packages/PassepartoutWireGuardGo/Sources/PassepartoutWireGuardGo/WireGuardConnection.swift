passepartoutvpn/passepartout

commit 7b8dbfe84a101ca7196a9f9821d06c0e3ec21c91

Minimize dependencies of VPN implementations (#1057)
### OpenVPN

- Make CPassepartoutCryptoOpenSSL agnostic of PassepartoutKit
- Move Allocation/ZeroingData from PassepartoutKit to package for
internal use
  - Rename ZeroingData.count to .length for consistency with NSData
  - Duplicate some Data manipulation code in CryptoOpenSSL
- Retain a simplified version of ZeroingData in PassepartoutKit
(AutoerasingData)

### WireGuard

- Make WireGuardKit imports `internal`

Packages/PassepartoutWireGuardGo/Sources/PassepartoutWireGuardGo/WireGuardConnection.swift
@@ -33,8 +33,8 @@ import Foundation
 import NetworkExtension
 import PassepartoutKit
 import os
-import WireGuardKit
-import WireGuardKitGo
+internal import WireGuardKit
+internal import WireGuardKitGo
 
 public final class WireGuardConnection: Connection {
     private let statusSubject: CurrentValueSubject<ConnectionStatus, Error>
@@ -50,7 +50,7 @@ public final class WireGuardConnection: Connection {
     private var dataCountTimer: AnyCancellable?
 
     private lazy var adapter: WireGuardAdapter = {
-        WireGuardAdapter(with: self, backend: WireGuardBackendGo()) { logLevel, message in
+        WireGuardAdapter(with: AdapterDelegate(connection: self), backend: WireGuardBackendGo()) { logLevel, message in
             pp_log(.wireguard, osLogLevel: logLevel.osLogLevel, message)
         }
     }()
@@ -156,34 +156,47 @@ public final class WireGuardConnection: Connection {
     }
 }
 
-extension WireGuardConnection: WireGuardAdapterDelegate {
-    public func adapterShouldReassert(_ adapter: WireGuardAdapter, reasserting: Bool) {
-        if reasserting {
-            statusSubject.send(.connecting)
-        }
-    }
+// MARK: - WireGuardAdapterDelegate
 
-    public func adapterShouldSetNetworkSettings(_ adapter: WireGuardAdapter, settings: NEPacketTunnelNetworkSettings, completionHandler: ((Error?) -> Void)?) {
-        let module = NESettingsModule(fullSettings: settings)
-        let addressObject = Address(rawValue: settings.tunnelRemoteAddress)
-        if addressObject == nil {
-            pp_log(.wireguard, .error, "Unable to parse remote tunnel address")
+private extension WireGuardConnection {
+    final class AdapterDelegate: WireGuardAdapterDelegate {
+        private weak var connection: WireGuardConnection?
+
+        init(connection: WireGuardConnection) {
+            self.connection = connection
         }
 
-        Task {
-            do {
-                try await controller.setTunnelSettings(with: TunnelRemoteInfo(
-                    originalModuleId: moduleId,
-                    address: addressObject,
-                    modules: [module]
-                ))
-                completionHandler?(nil)
-                pp_log(.wireguard, .info, "Tunnel interface is now UP")
-                statusSubject.send(.connected)
-            } catch {
-                completionHandler?(error)
-                pp_log(.wireguard, .error, "Unable to configure tunnel settings: \(error)")
-                statusSubject.send(.disconnected)
+        func adapterShouldReassert(_ adapter: WireGuardAdapter, reasserting: Bool) {
+            if reasserting {
+                connection?.statusSubject.send(.connecting)
+            }
+        }
+
+        func adapterShouldSetNetworkSettings(_ adapter: WireGuardAdapter, settings: NEPacketTunnelNetworkSettings, completionHandler: ((Error?) -> Void)?) {
+            guard let connection else {
+                return
+            }
+            let module = NESettingsModule(fullSettings: settings)
+            let addressObject = Address(rawValue: settings.tunnelRemoteAddress)
+            if addressObject == nil {
+                pp_log(.wireguard, .error, "Unable to parse remote tunnel address")
+            }
+
+            Task {
+                do {
+                    try await connection.controller.setTunnelSettings(with: TunnelRemoteInfo(
+                        originalModuleId: connection.moduleId,
+                        address: addressObject,
+                        modules: [module]
+                    ))
+                    completionHandler?(nil)
+                    pp_log(.wireguard, .info, "Tunnel interface is now UP")
+                    connection.statusSubject.send(.connected)
+                } catch {
+                    completionHandler?(error)
+                    pp_log(.wireguard, .error, "Unable to configure tunnel settings: \(error)")
+                    connection.statusSubject.send(.disconnected)
+                }
             }
         }
     }

Packages/App/Sources/UILibrary/L10n/Modules/OpenVPNModule+L10n.swift
@@ -133,10 +133,10 @@ extension OpenVPN.XORMethod: StyledLocalizableEntity {
     private var longDescription: String {
         switch self {
         case .xormask(let mask):
-            return "\(shortDescription) \(mask.zData.toHex())"
+            return "\(shortDescription) \(mask.toHex())"
 
         case .obfuscate(let mask):
-            return "\(shortDescription) \(mask.zData.toHex())"
+            return "\(shortDescription) \(mask.toHex())"
 
         default:
             return shortDescription

Packages/PassepartoutOpenVPNOpenSSL/Package.swift
@@ -31,14 +31,14 @@ let package = Package(
     targets: [
         .target(
             name: "CPassepartoutCryptoOpenSSL",
-            dependencies: [
-                "openssl-apple",
-                "PassepartoutKit-Framework"
-            ]
+            dependencies: ["openssl-apple",]
         ),
         .target(
             name: "CPassepartoutOpenVPNOpenSSL",
-            dependencies: ["CPassepartoutCryptoOpenSSL"],
+            dependencies: [
+                "CPassepartoutCryptoOpenSSL",
+                "PassepartoutKit-Framework"
+            ],
             exclude: [
                 "lib/COPYING",
                 "lib/Makefile",
@@ -58,7 +58,7 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "PassepartoutCryptoOpenSSLTests",
+            name: "CPassepartoutCryptoOpenSSLTests",
             dependencies: ["PassepartoutCryptoOpenSSL"]
         ),
         .testTarget(

Packages/PassepartoutOpenVPNOpenSSL/Sources/CPassepartoutCryptoOpenSSL/Crypto.m
@@ -25,10 +25,4 @@
 
 #import "Crypto.h"
 
-#define MAX_BLOCK_SIZE  16  // AES only, block is 128-bit
-
-size_t pp_alloc_crypto_capacity(size_t size, size_t overhead) {
-
-    // encryption, byte-alignment, overhead (e.g. IV, digest)
-    return 2 * size + MAX_BLOCK_SIZE + overhead;
-}
+NSString *const PassepartoutCryptoErrorDomain = @"PassepartoutCrypto";

Packages/PassepartoutOpenVPNOpenSSL/Sources/CPassepartoutCryptoOpenSSL/CryptoAEAD.m
@@ -34,11 +34,12 @@
 //      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#import <PassepartoutKit/PassepartoutKit.h>
 #import <openssl/evp.h>
 
+#import "Allocation.h"
+#import "Crypto.h"
 #import "CryptoAEAD.h"
-#import "CryptoMacros.h"
+#import "ZeroingData.h"
 
 @interface CryptoAEAD ()
 
@@ -75,8 +76,8 @@
 
         self.cipherCtxEnc = EVP_CIPHER_CTX_new();
         self.cipherCtxDec = EVP_CIPHER_CTX_new();
-        self.cipherIVEnc = pp_alloc(self.cipherIVLength);
-        self.cipherIVDec = pp_alloc(self.cipherIVLength);
+        self.cipherIVEnc = pp_alloc_crypto(self.cipherIVLength);
+        self.cipherIVDec = pp_alloc_crypto(self.cipherIVLength);
 
         self.mappedError = ^NSError *(CryptoAEADError errorCode) {
             return [NSError errorWithDomain:PassepartoutCryptoErrorDomain code:0 userInfo:nil];
@@ -116,7 +117,7 @@
 
 - (void)configureEncryptionWithCipherKey:(ZeroingData *)cipherKey hmacKey:(ZeroingData *)hmacKey
 {
-    NSParameterAssert(cipherKey.count >= self.cipherKeyLength);
+    NSParameterAssert(cipherKey.length >= self.cipherKeyLength);
     NSParameterAssert(hmacKey);
 
     EVP_CIPHER_CTX_reset(self.cipherCtxEnc);
@@ -157,7 +158,7 @@
 
 - (void)configureDecryptionWithCipherKey:(ZeroingData *)cipherKey hmacKey:(ZeroingData *)hmacKey
 {
-    NSParameterAssert(cipherKey.count >= self.cipherKeyLength);
+    NSParameterAssert(cipherKey.length >= self.cipherKeyLength);
     NSParameterAssert(hmacKey);
 
     EVP_CIPHER_CTX_reset(self.cipherCtxDec);

Packages/PassepartoutOpenVPNOpenSSL/Sources/CPassepartoutCryptoOpenSSL/CryptoCBC.m
@@ -34,12 +34,13 @@
 //      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#import <PassepartoutKit/PassepartoutKit.h>
 #import <openssl/evp.h>
 #import <openssl/rand.h>
 
+#import "Allocation.h"
+#import "Crypto.h"
 #import "CryptoCBC.h"
-#import "CryptoMacros.h"
+#import "ZeroingData.h"
 
 const NSInteger CryptoCBCMaxHMACLength = 100;
 
@@ -103,7 +104,7 @@ const NSInteger CryptoCBCMaxHMACLength = 100;
         macParams[1] = OSSL_PARAM_construct_end();
         self.macParams = macParams;
 
-        self.bufferDecHMAC = pp_alloc(CryptoCBCMaxHMACLength);
+        self.bufferDecHMAC = pp_alloc_crypto(CryptoCBCMaxHMACLength);
 
         self.mappedError = ^NSError *(CryptoCBCError errorCode) {
             return [NSError errorWithDomain:PassepartoutCryptoErrorDomain code:0 userInfo:nil];
@@ -147,16 +148,16 @@ const NSInteger CryptoCBCMaxHMACLength = 100;
 - (void)configureEncryptionWithCipherKey:(ZeroingData *)cipherKey hmacKey:(ZeroingData *)hmacKey
 {
     NSParameterAssert(hmacKey);
-    NSParameterAssert(hmacKey.count >= self.hmacKeyLength);
+    NSParameterAssert(hmacKey.length >= self.hmacKeyLength);
 
     if (self.cipher) {
-        NSParameterAssert(cipherKey.count >= self.cipherKeyLength);
+        NSParameterAssert(cipherKey.length >= self.cipherKeyLength);
 
         EVP_CIPHER_CTX_reset(self.cipherCtxEnc);
         EVP_CipherInit(self.cipherCtxEnc, self.cipher, cipherKey.bytes, NULL, 1);
     }
 
-    self.hmacKeyEnc = [[ZeroingData alloc] initWithBytes:hmacKey.bytes count:self.hmacKeyLength];
+    self.hmacKeyEnc = [[ZeroingData alloc] initWithBytes:hmacKey.bytes length:self.hmacKeyLength];
 }
 
 - (BOOL)encryptBytes:(const uint8_t *)bytes length:(NSInteger)length dest:(uint8_t *)dest destLength:(NSInteger *)destLength flags:(const CryptoFlags * _Nullable)flags error:(NSError * _Nullable __autoreleasing * _Nullable)error
@@ -188,7 +189,7 @@ const NSInteger CryptoCBCMaxHMACLength = 100;
         l1 = (int)length;
     }
     EVP_MAC_CTX *ctx = EVP_MAC_CTX_new(self.mac);
-    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyEnc.bytes, self.hmacKeyEnc.count, self.macParams);
+    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyEnc.bytes, self.hmacKeyEnc.length, self.macParams);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, outIV, l1 + l2 + self.cipherIVLength);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_final(ctx, dest, &l3, self.digestLength);
     EVP_MAC_CTX_free(ctx);
@@ -203,16 +204,16 @@ const NSInteger CryptoCBCMaxHMACLength = 100;
 - (void)configureDecryptionWithCipherKey:(ZeroingData *)cipherKey hmacKey:(ZeroingData *)hmacKey
 {
     NSParameterAssert(hmacKey);
-    NSParameterAssert(hmacKey.count >= self.hmacKeyLength);
+    NSParameterAssert(hmacKey.length >= self.hmacKeyLength);
 
     if (self.cipher) {
-        NSParameterAssert(cipherKey.count >= self.cipherKeyLength);
+        NSParameterAssert(cipherKey.length >= self.cipherKeyLength);
 
         EVP_CIPHER_CTX_reset(self.cipherCtxDec);
         EVP_CipherInit(self.cipherCtxDec, self.cipher, cipherKey.bytes, NULL, 0);
     }
 
-    self.hmacKeyDec = [[ZeroingData alloc] initWithBytes:hmacKey.bytes count:self.hmacKeyLength];
+    self.hmacKeyDec = [[ZeroingData alloc] initWithBytes:hmacKey.bytes length:self.hmacKeyLength];
 }
 
 - (BOOL)decryptBytes:(const uint8_t *)bytes length:(NSInteger)length dest:(uint8_t *)dest destLength:(NSInteger *)destLength flags:(const CryptoFlags * _Nullable)flags error:(NSError * _Nullable __autoreleasing * _Nullable)error
@@ -223,7 +224,7 @@ const NSInteger CryptoCBCMaxHMACLength = 100;
     int code = 1;
 
     EVP_MAC_CTX *ctx = EVP_MAC_CTX_new(self.mac);
-    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyDec.bytes, self.hmacKeyDec.count, self.macParams);
+    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyDec.bytes, self.hmacKeyDec.length, self.macParams);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, bytes + self.digestLength, length - self.digestLength);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_final(ctx, self.bufferDecHMAC, &l1, self.digestLength);
     EVP_MAC_CTX_free(ctx);
@@ -257,7 +258,7 @@ const NSInteger CryptoCBCMaxHMACLength = 100;
     int code = 1;
 
     EVP_MAC_CTX *ctx = EVP_MAC_CTX_new(self.mac);
-    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyDec.bytes, self.hmacKeyDec.count, self.macParams);
+    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyDec.bytes, self.hmacKeyDec.length, self.macParams);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, bytes + self.digestLength, length - self.digestLength);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_final(ctx, self.bufferDecHMAC, &l1, self.digestLength);
     EVP_MAC_CTX_free(ctx);

Packages/PassepartoutOpenVPNOpenSSL/Sources/CPassepartoutCryptoOpenSSL/CryptoCTR.m
@@ -23,11 +23,12 @@
 //  along with PassepartoutKit.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#import <PassepartoutKit/PassepartoutKit.h>
 #import <openssl/evp.h>
 
+#import "Allocation.h"
+#import "Crypto.h"
 #import "CryptoCTR.h"
-#import "CryptoMacros.h"
+#import "ZeroingData.h"
 
 @interface CryptoCTR ()
 
@@ -92,7 +93,7 @@
         macParams[1] = OSSL_PARAM_construct_end();
         self.macParams = macParams;
 
-        self.bufferDecHMAC = pp_alloc(self.tagLength);
+        self.bufferDecHMAC = pp_alloc_crypto(self.tagLength);
 
         self.mappedError = ^NSError *(CryptoCTRError errorCode) {
             return [NSError errorWithDomain:PassepartoutCryptoErrorDomain code:0 userInfo:nil];
@@ -137,13 +138,13 @@
 - (void)configureEncryptionWithCipherKey:(ZeroingData *)cipherKey hmacKey:(ZeroingData *)hmacKey
 {
     NSParameterAssert(hmacKey);
-    NSParameterAssert(hmacKey.count >= self.hmacKeyLength);
-    NSParameterAssert(cipherKey.count >= self.cipherKeyLength);
+    NSParameterAssert(hmacKey.length >= self.hmacKeyLength);
+    NSParameterAssert(cipherKey.length >= self.cipherKeyLength);
 
     EVP_CIPHER_CTX_reset(self.cipherCtxEnc);
     EVP_CipherInit(self.cipherCtxEnc, self.cipher, cipherKey.bytes, NULL, 1);
 
-    self.hmacKeyEnc = [[ZeroingData alloc] initWithBytes:hmacKey.bytes count:self.hmacKeyLength];
+    self.hmacKeyEnc = [[ZeroingData alloc] initWithBytes:hmacKey.bytes length:self.hmacKeyLength];
 }
 
 - (BOOL)encryptBytes:(const uint8_t *)bytes length:(NSInteger)length dest:(uint8_t *)dest destLength:(NSInteger *)destLength flags:(const CryptoFlags * _Nullable)flags error:(NSError * _Nullable __autoreleasing * _Nullable)error
@@ -156,7 +157,7 @@
     int code = 1;
 
     EVP_MAC_CTX *ctx = EVP_MAC_CTX_new(self.mac);
-    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyEnc.bytes, self.hmacKeyEnc.count, self.macParams);
+    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyEnc.bytes, self.hmacKeyEnc.length, self.macParams);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, flags->ad, flags->adLength);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, bytes, length);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_final(ctx, dest, &l3, self.digestLength);
@@ -178,13 +179,13 @@
 - (void)configureDecryptionWithCipherKey:(ZeroingData *)cipherKey hmacKey:(ZeroingData *)hmacKey
 {
     NSParameterAssert(hmacKey);
-    NSParameterAssert(hmacKey.count >= self.hmacKeyLength);
-    NSParameterAssert(cipherKey.count >= self.cipherKeyLength);
+    NSParameterAssert(hmacKey.length >= self.hmacKeyLength);
+    NSParameterAssert(cipherKey.length >= self.cipherKeyLength);
 
     EVP_CIPHER_CTX_reset(self.cipherCtxDec);
     EVP_CipherInit(self.cipherCtxDec, self.cipher, cipherKey.bytes, NULL, 0);
 
-    self.hmacKeyDec = [[ZeroingData alloc] initWithBytes:hmacKey.bytes count:self.hmacKeyLength];
+    self.hmacKeyDec = [[ZeroingData alloc] initWithBytes:hmacKey.bytes length:self.hmacKeyLength];
 }
 
 - (BOOL)decryptBytes:(const uint8_t *)bytes length:(NSInteger)length dest:(uint8_t *)dest destLength:(NSInteger *)destLength flags:(const CryptoFlags * _Nullable)flags error:(NSError * _Nullable __autoreleasing * _Nullable)error
@@ -205,7 +206,7 @@
     *destLength = l1 + l2;
 
     EVP_MAC_CTX *ctx = EVP_MAC_CTX_new(self.mac);
-    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyDec.bytes, self.hmacKeyDec.count, self.macParams);
+    CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_init(ctx, self.hmacKeyDec.bytes, self.hmacKeyDec.length, self.macParams);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, flags->ad, flags->adLength);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_update(ctx, dest, *destLength);
     CRYPTO_OPENSSL_TRACK_STATUS(code) EVP_MAC_final(ctx, self.bufferDecHMAC, &l3, self.digestLength);

Packages/PassepartoutOpenVPNOpenSSL/Sources/CPassepartoutCryptoOpenSSL/ZeroingData.m
@@ -1,0 +1,319 @@
+//
+//  ZeroingData.m
+//  PassepartoutKit
+//
+//  Created by Davide De Rosa on 4/28/17.
+//  Copyright (c) 2024 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of PassepartoutKit.
+//
+//  PassepartoutKit is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  PassepartoutKit is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with PassepartoutKit.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  This file incorporates work covered by the following copyright and
+//  permission notice:
+//
+//      Copyright (c) 2018-Present Private Internet Access
+//
+//      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "ZeroingData.h"
+#import "Allocation.h"
+
+@interface ZeroingData () {
+    uint8_t *_bytes;
+}
+
+@end
+
+@implementation ZeroingData
+
+- (instancetype)init
+{
+    return [self initWithBytes:NULL length:0];
+}
+
+- (instancetype)initWithLength:(NSInteger)length
+{
+    if ((self = [super init])) {
+        _length = length;
+        _bytes = pp_alloc_crypto(length);
+        bzero(_bytes, _length);
+    }
+    return self;
+}
+
+- (instancetype)initWithBytes:(const uint8_t *)bytes length:(NSInteger)length
+{
+//    NSParameterAssert(bytes);
+
+    if ((self = [super init])) {
+        _length = length;
+        _bytes = pp_alloc_crypto(length);
+        memcpy(_bytes, bytes, length);
+    }
+    return self;
+}
+
+- (instancetype)initWithBytesNoCopy:(uint8_t *)bytes length:(NSInteger)length
+{
+    NSParameterAssert(bytes);
+
+    if ((self = [super init])) {
+        _length = length;
+        _bytes = bytes;
+    }
+    return self;
+}
+
+- (instancetype)initWithUInt8:(uint8_t)uint8
+{
+    if ((self = [super init])) {
+        _length = 1;
+        _bytes = pp_alloc_crypto(_length);
+        _bytes[0] = uint8;
+    }
+    return self;
+}
+
+- (instancetype)initWithUInt16:(uint16_t)uint16
+{
+    if ((self = [super init])) {
+        _length = 2;
+        _bytes = pp_alloc_crypto(_length);
+        _bytes[0] = (uint16 & 0xff);
+        _bytes[1] = (uint16 >> 8);
+    }
+    return self;
+}
+
+- (instancetype)initWithData:(NSData *)data
+{
+    return [self initWithData:data offset:0 length:data.length];
+}
+
+- (instancetype)initWithData:(NSData *)data offset:(NSInteger)offset length:(NSInteger)length
+{
+    NSParameterAssert(data);
+    NSParameterAssert(length >= 0);
+    NSParameterAssert(offset + length <= data.length);
+
+    if ((self = [super init])) {
+        _length = length;
+        _bytes = pp_alloc_crypto(length);
+        memcpy(_bytes, data.bytes + offset, length);
+    }
+    return self;
+}
+
+- (instancetype)initWithString:(NSString *)string nullTerminated:(BOOL)nullTerminated
+{
+    NSParameterAssert(string);
+
+    if ((self = [super init])) {
+        const int stringLength = (int)string.length;
+        _length = stringLength + (nullTerminated ? 1 : 0);
+        _bytes = pp_alloc_crypto(_length);
+
+        const char *stringBytes = [string cStringUsingEncoding:NSUTF8StringEncoding];
+        if (stringBytes) {
+            memcpy(_bytes, stringBytes, stringLength);
+        } else {
+            NSAssert(stringBytes != NULL, @"Cannot encode string to UTF-8");
+            bzero(_bytes, stringLength);
+        }
+        if (nullTerminated) {
+            _bytes[stringLength] = '\0';
+        }
+    }
+    return self;
+}
+
+- (instancetype)copy
+{
+    return [[ZeroingData alloc] initWithBytes:_bytes length:_length];
+}
+
+- (void)dealloc
+{
+    bzero(_bytes, _length);
+    free(_bytes);
+}
+
+- (const uint8_t *)bytes
+{
+    return _bytes;
+}
+
+- (uint8_t *)mutableBytes
+{
+    return _bytes;
+}
+
+- (void)appendData:(ZeroingData *)other
+{
+    NSParameterAssert(other);
+
+    const NSInteger newLength = _length + other.length;
+    uint8_t *newBytes = pp_alloc_crypto(newLength);
+    memcpy(newBytes, _bytes, _length);
+    memcpy(newBytes + _length, other.bytes, other.length);
+    
+    bzero(_bytes, _length);
+    free(_bytes);
+    
+    _bytes = newBytes;
+    _length = newLength;
+}
+
+- (void)truncateToSize:(NSInteger)size
+{
+    NSParameterAssert(size <= _length);
+    
+    uint8_t *newBytes = pp_alloc_crypto(size);
+    memcpy(newBytes, _bytes, size);
+
+    bzero(_bytes, _length);
+    free(_bytes);
+    
+    _bytes = newBytes;
+    _length = size;
+}
+
+- (void)removeUntilOffset:(NSInteger)until
+{
+    NSParameterAssert(until <= _length);
+    
+    const NSInteger newLength = _length - until;
+    uint8_t *newBytes = pp_alloc_crypto(newLength);
+    memcpy(newBytes, _bytes + until, newLength);
+    
+    bzero(_bytes, _length);
+    free(_bytes);
+    
+    _bytes = newBytes;
+    _length = newLength;
+}
+
+- (void)zero
+{
+    bzero(_bytes, _length);
+}
+
+- (ZeroingData *)appendingData:(ZeroingData *)other
+{
+    NSParameterAssert(other);
+
+    const NSInteger newLength = _length + other.length;
+    uint8_t *newBytes = pp_alloc_crypto(newLength);
+    memcpy(newBytes, _bytes, _length);
+    memcpy(newBytes + _length, other.bytes, other.length);
+    
+    return [[ZeroingData alloc] initWithBytesNoCopy:newBytes length:newLength];
+}
+
+- (ZeroingData *)withOffset:(NSInteger)offset length:(NSInteger)length
+{
+    NSParameterAssert(offset + length <= _length);
+
+    uint8_t *newBytes = pp_alloc_crypto(length);
+    memcpy(newBytes, _bytes + offset, length);
+    
+    return [[ZeroingData alloc] initWithBytesNoCopy:newBytes length:length];
+}
+
+- (uint16_t)UInt16ValueFromOffset:(NSInteger)from
+{
+    NSParameterAssert(from + 2 <= _length);
+
+    uint16_t value = 0;
+    value |= _bytes[from];
+    value |= _bytes[from + 1] << 8;
+    return value;
+}
+
+- (uint16_t)networkUInt16ValueFromOffset:(NSInteger)from
+{
+    NSParameterAssert(from + 2 <= _length);
+    
+    uint16_t value = 0;
+    value |= _bytes[from];
+    value |= _bytes[from + 1] << 8;
+    return CFSwapInt16BigToHost(value);
+}
+
+- (NSString *)nullTerminatedStringFromOffset:(NSInteger)from
+{
+    NSParameterAssert(from <= _length);
+
+    NSInteger nullOffset = NSNotFound;
+    for (NSInteger i = from; i < _length; ++i) {
+        if (_bytes[i] == 0) {
+            nullOffset = i;
+            break;
+        }
+    }
+    if (nullOffset == NSNotFound) {
+        return nil;
+    }
+    const NSInteger stringLength = nullOffset - from;
+    return [[NSString alloc] initWithBytes:_bytes length:stringLength encoding:NSUTF8StringEncoding];
+}
+
+- (BOOL)isEqual:(id)object
+{
+    NSParameterAssert(object);
+
+    if (![object isKindOfClass:[ZeroingData class]]) {
+        return NO;
+    }
+    ZeroingData *other = (ZeroingData *)object;
+    if (other.length != _length) {
+        return NO;
+    }
+    return !memcmp(_bytes, other.bytes, _length);
+}
+
+- (BOOL)isEqualToData:(NSData *)data
+{
+    NSParameterAssert(data);
+
+    if (data.length != _length) {
+        return NO;
+    }
+    return !memcmp(_bytes, data.bytes, _length);
+}
+
+- (NSData *)toData
+{
+    return [NSData dataWithBytes:_bytes length:_length];
+}
+
+- (NSString *)toHex
+{
+    const NSUInteger capacity = _length * 2;
+    NSMutableString *hexString = [[NSMutableString alloc] initWithCapacity:capacity];
+    for (int i = 0; i < _length; ++i) {
+        [hexString appendFormat:@"%02x", _bytes[i]];
+    }
+    return hexString;
+}
+
+@end

commit 11eeb40f45bd424e52dfefd729e5a63709a0d3cf

Retain WireGuardAdapterDelegate
Regression in #1057

Packages/PassepartoutWireGuardGo/Sources/PassepartoutWireGuardGo/WireGuardConnection.swift
@@ -50,10 +50,12 @@ public final class WireGuardConnection: Connection {
     private var dataCountTimer: AnyCancellable?
 
     private lazy var adapter: WireGuardAdapter = {
-        WireGuardAdapter(with: AdapterDelegate(connection: self), backend: WireGuardBackendGo()) { logLevel, message in
+        WireGuardAdapter(with: delegate, backend: WireGuardBackendGo()) { logLevel, message in
             pp_log(.wireguard, osLogLevel: logLevel.osLogLevel, message)
         }
     }()
+
+    private lazy var delegate: WireGuardAdapterDelegate = AdapterDelegate(connection: self)
 
     public init(
         parameters: ConnectionParameters,