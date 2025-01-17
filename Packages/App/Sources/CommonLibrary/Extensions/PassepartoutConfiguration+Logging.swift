47051ce5e2b4db82077969b963e2f21e76def879
Davide <keeshux@gmail.com>
2025-01-17 13:21:00 +0100

    Move logging methods to Kit
    - Append and rotate logs by size (500k)
    - Add marker between app/tunnel launches
    - Purge logs on each save (3 days)
    - Unify debug log content view across platforms
        - macOS: Table + inspect full line
        - iOS/tvOS: Use List

--- Packages/App/Sources/CommonLibrary/Extensions/PassepartoutConfiguration+Logging.swift
+++ Packages/App/Sources/CommonLibrary/Extensions/PassepartoutConfiguration+Logging.swift
@@ -30,67 +30,27 @@ extension PassepartoutConfiguration {
     public func configureLogging(to url: URL, parameters: Constants.Log, logsPrivateData: Bool) {
         pp_log(.app, .debug, "Log to: \(url)")
 
-        setLocalLogger(options: .init(
+        setLocalLogger(
             url: url,
-            maxNumberOfLines: parameters.maxNumberOfLines,
-            maxLevel: parameters.maxLevel,
+            options: parameters.options,
             mapper: parameters.formatter.formattedLine
-        ))
+        )
 
         if logsPrivateData {
             logsAddresses = true
             logsModules = true
         }
 
-        if let maxAge = parameters.maxAge {
-            purgeLogs(at: url, beyond: maxAge)
-        }
+        appendLog(parameters.options.maxLevel, message: "")
+        appendLog(parameters.options.maxLevel, message: "--- BEGIN ---")
+        appendLog(parameters.options.maxLevel, message: "")
     }
 
     public func currentLog(parameters: Constants.Log) -> [String] {
         currentLogLines(
             sinceLast: parameters.sinceLast,
-            maxLevel: parameters.maxLevel
+            maxLevel: parameters.options.maxLevel
         )
         .map(parameters.formatter.formattedLine)
-    }
-
-    public func availableLogs(at url: URL) -> [Date: URL] {
-        let parent = url.deletingLastPathComponent()
-        let prefix = url.lastPathComponent
-        do {
-            let contents = try FileManager.default.contentsOfDirectory(at: parent, includingPropertiesForKeys: nil)
-            return contents.reduce(into: [:]) { found, item in
-                let filename = item.lastPathComponent
-                guard filename.hasPrefix(prefix) else {
-                    return
-                }
-                guard let timestampString = filename.split(separator: ".").last,
-                      let timestamp = TimeInterval(timestampString) else {
-                    return
-                }
-                let date = Date(timeIntervalSince1970: timestamp)
-                found[date] = item
-            }
-        } catch {
-            return [:]
-        }
-    }
-
-    public func flushLog() {
-        try? saveLog()
-    }
-}
-
-private extension PassepartoutConfiguration {
-    func purgeLogs(at url: URL, beyond maxAge: TimeInterval) {
-        let logs = availableLogs(at: url)
-        let minDate = Date().addingTimeInterval(-maxAge)
-        logs.forEach { date, url in
-            guard date >= minDate else {
-                try? FileManager.default.removeItem(at: url)
-                return
-            }
-        }
     }
 }
