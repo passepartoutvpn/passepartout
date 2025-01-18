b6528d5fe0df073a3ed015319e3d8a83844cd2d4
Davide <keeshux@gmail.com>
2025-01-18 18:09:55 +0100

    Skip value regex

--- ci/version-number.sh
+++ ci/version-number.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cwd=`dirname $0`
 xcconfig="$cwd/../Passepartout/Config.xcconfig"
-$cwd/xcconfig-get.sh $xcconfig MARKETING_VERSION "([0-9]\.[0-9]\.[0-9])"
+$cwd/xcconfig-get.sh $xcconfig MARKETING_VERSION
--- ci/xcconfig-get.sh
+++ ci/xcconfig-get.sh
@@ -7,11 +7,6 @@ if [[ -z "$2" ]]; then
     echo "Setting key required"
     exit 1
 fi
-if [[ -z "$3" ]]; then
-    echo "Setting regex required"
-    exit 1
-fi
 xcconfig="$1"
 setting_key="$2"
-setting_pattern="$3"
-grep $setting_key $xcconfig | sed -E "s/^.*${setting_key} = ${setting_pattern}/\1/"
+grep $setting_key $xcconfig | sed -E "s/^.*${setting_key} = (.*)$/\1/"
