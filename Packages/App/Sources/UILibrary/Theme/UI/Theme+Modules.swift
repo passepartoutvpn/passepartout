3e952578fb3f9b4124e1c3f3ecb8e452c5389b68
Davide <keeshux@gmail.com>
2025-01-30 15:20:53 +0100

    Create views for module fields

---
+++ Packages/App/Sources/UILibrary/Theme/UI/Theme+Modules.swift
@@ -0,0 +1,164 @@
+//
+//  Theme+Modules.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 1/30/25.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import CommonUtils
+import SwiftUI
+
+public struct ThemeModuleText: View {
+    private let caption: String
+
+    private let value: String?
+
+    public init(caption: String, value: String? = nil) {
+        self.caption = caption
+        self.value = value
+    }
+
+    public var body: some View {
+        Text(caption)
+            .themeTrailingValue(value)
+    }
+}
+
+public struct ThemeModuleTextField: View {
+    private let caption: String
+
+    @Binding
+    private var value: String
+
+    private let placeholder: String
+
+    public init(caption: String, value: Binding<String>, placeholder: String) {
+        self.caption = caption
+        _value = value
+        self.placeholder = placeholder
+    }
+
+    public var body: some View {
+        ThemeTextField(caption, text: $value, placeholder: placeholder)
+    }
+}
+
+public struct ThemeModuleTextList: View {
+    private let caption: String
+
+    private let values: [String]
+
+    public init(caption: String, values: [String]) {
+        self.caption = caption
+        self.values = values
+    }
+
+    public var body: some View {
+        if !values.isEmpty {
+            NavigationLink(caption) {
+                Form {
+                    ForEach(Array(values.enumerated()), id: \.offset) {
+                        Text($0.element)
+                    }
+                }
+                .navigationTitle(caption)
+                .themeForm()
+            }
+        } else {
+            Text(caption)
+                .themeTrailingValue(Strings.Global.Nouns.empty)
+        }
+    }
+}
+
+public struct ThemeModuleCopiableText: View {
+    private let caption: String
+
+    private let value: String
+
+    private let multiline: Bool
+
+    public init(caption: String, value: String, multiline: Bool = true) {
+        self.caption = caption
+        self.value = value
+        self.multiline = multiline
+    }
+
+    public var body: some View {
+        ThemeCopiableText(title: caption, value: value, isMultiLine: multiline) {
+            Text($0)
+        }
+    }
+}
+
+public struct ThemeModuleLongContent: View {
+    private let title: String
+
+    private let content: String
+
+    public init(title: String, content: String) {
+        self.title = title
+        self.content = content
+    }
+
+    public var body: some View {
+        LongContentLink(title, content: .constant(content)) {
+            Text($0)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+public struct ThemeModuleLongContentPreview: View {
+    private let title: String
+
+    private let content: String
+
+    private let preview: String?
+
+    public init(title: String, content: String, preview: String?) {
+        self.title = title
+        self.content = content
+        self.preview = preview
+    }
+
+    public var body: some View {
+        LongContentLink(title, content: .constant(content), preview: preview) {
+            Text(preview != nil ? $0 : "")
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+public struct ThemeModulePush: View {
+    private let caption: String
+
+    private let route: any Hashable
+
+    public init(caption: String, route: any Hashable) {
+        self.caption = caption
+        self.route = route
+    }
+
+    public var body: some View {
+        NavigationLink(caption, value: route)
+    }
+}
