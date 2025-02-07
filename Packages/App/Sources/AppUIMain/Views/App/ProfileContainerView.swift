passepartoutvpn/passepartout

commit 0132aa79cc3f97ea468814dfb77595859eacf949

Delay profile verifications (#1135)
Fixes #1070

Packages/App/Sources/AppUIMain/Views/App/ProfileContainerView.swift
@@ -29,6 +29,10 @@ import PassepartoutKit
 import SwiftUI
 
 struct ProfileContainerView: View, Routable {
+
+    @EnvironmentObject
+    private var iapManager: IAPManager
+
     let layout: ProfilesLayout
 
     let profileManager: ProfileManager
@@ -71,6 +75,7 @@ private extension ProfileContainerView {
             ProfileListView(
                 profileManager: profileManager,
                 tunnel: tunnel,
+                isVerifying: isVerifying,
                 errorHandler: errorHandler,
                 flow: flow
             )
@@ -79,10 +84,15 @@ private extension ProfileContainerView {
             ProfileGridView(
                 profileManager: profileManager,
                 tunnel: tunnel,
+                isVerifying: isVerifying,
                 errorHandler: errorHandler,
                 flow: flow
             )
         }
+    }
+
+    var isVerifying: Bool {
+        iapManager.isLoadingReceipt
     }
 }
 
@@ -99,9 +109,8 @@ private struct ContainerModifier: ViewModifier {
     func body(content: Content) -> some View {
         debugChanges()
         return content
-            .themeProgress(
-                if: !profileManager.isReady,
-                isEmpty: !profileManager.hasProfiles,
+            .themeEmpty(
+                if: !profileManager.hasProfiles,
                 emptyContent: emptyView
             )
             .searchable(text: $search)

Packages/App/Sources/AppUIMain/Views/App/AddProfileMenu.swift
@@ -67,7 +67,7 @@ private extension AddProfileMenu {
         Button {
             isImporting = true
         } label: {
-            ThemeImageLabel(Strings.Views.App.Toolbar.importProfile.withTrailingDots, .profileImport)
+            ThemeImageLabel(Strings.Views.App.Toolbar.importProfile.forMenu, .profileImport)
         }
     }
 
@@ -93,7 +93,7 @@ private extension AddProfileMenu {
 
     var migrateProfilesButton: some View {
         Button(action: onMigrateProfiles) {
-            ThemeImageLabel(Strings.Views.App.Toolbar.migrateProfiles.withTrailingDots, .profileMigrate)
+            ThemeImageLabel(Strings.Views.App.Toolbar.migrateProfiles.forMenu, .profileMigrate)
         }
     }
 }

Packages/App/Sources/AppUIMain/Views/App/AppCoordinator.swift
@@ -268,6 +268,13 @@ extension AppCoordinator {
     }
 
     public func onPurchaseRequired(_ features: Set<AppFeature>) {
+        guard !iapManager.isLoadingReceipt else {
+            errorHandler.handle(
+                title: Strings.Views.Paywall.Alerts.Verifying.title,
+                message: Strings.Views.Paywall.Alerts.Verifying.message
+            )
+            return
+        }
         pp_log(.app, .info, "Present paywall for features: \(features)")
         setLater(.init(features, needsConfirmation: true)) {
             paywallReason = $0

Packages/App/Sources/AppUIMain/Views/App/ProfileContextMenu.swift
@@ -146,7 +146,7 @@ private extension ProfileContextMenu {
 private extension Profile {
     var providerServerSelectionTitle: String {
         (attributes.isAvailableForTV == true ?
-         Strings.Views.Providers.selectEntity : Strings.Views.App.ProfileContext.connectTo).withTrailingDots
+         Strings.Views.Providers.selectEntity : Strings.Views.App.ProfileContext.connectTo).forMenu
     }
 }
 

Packages/App/Sources/AppUIMain/Views/App/ProfileGridView.swift
@@ -39,6 +39,8 @@ struct ProfileGridView: View, Routable, TunnelInstallationProviding {
     @ObservedObject
     var tunnel: ExtendedTunnel
 
+    let isVerifying: Bool
+
     let errorHandler: ErrorHandler
 
     var flow: ProfileFlow?
@@ -69,7 +71,9 @@ struct ProfileGridView: View, Routable, TunnelInstallationProviding {
                                 }
                             }
                     }
-                    .themeGridHeader(title: Strings.Views.App.Folders.default)
+                    .themeGridHeader {
+                        VerificationView(isVerifying: isVerifying)
+                    }
                 }
                 .padding(.horizontal)
             }
@@ -148,6 +152,7 @@ private extension ProfileGridView {
     ProfileGridView(
         profileManager: .forPreviews,
         tunnel: .forPreviews,
+        isVerifying: false,
         errorHandler: .default()
     )
     .themeWindow(width: 600, height: 300)

Packages/App/Sources/AppUIMain/Views/App/ProfileListView.swift
@@ -45,6 +45,8 @@ struct ProfileListView: View, Routable, TunnelInstallationProviding {
     @ObservedObject
     var tunnel: ExtendedTunnel
 
+    let isVerifying: Bool
+
     let errorHandler: ErrorHandler
 
     var flow: ProfileFlow?
@@ -59,15 +61,16 @@ struct ProfileListView: View, Routable, TunnelInstallationProviding {
                 headerView
                     .unanimated()
             }
-            Group {
+            Section {
                 ForEach(allPreviews, content: profileView)
                     .onDelete { offsets in
                         Task {
                             await profileManager.removeProfiles(at: offsets)
                         }
                     }
+            } header: {
+                VerificationView(isVerifying: isVerifying)
             }
-            .themeSection(header: Strings.Views.App.Folders.default)
         }
         .themeForm()
         .themeAnimation(on: profileManager.isReady, category: .profiles)
@@ -145,6 +148,7 @@ private extension ProfileListView {
     ProfileListView(
         profileManager: .forPreviews,
         tunnel: .forPreviews,
+        isVerifying: false,
         errorHandler: .default()
     )
     .withMockEnvironment()

Packages/App/Sources/AppUIMain/Views/App/VerificationView.swift
@@ -1,8 +1,8 @@
 //
-//  AppUserLevel.swift
+//  VerificationView.swift
 //  Passepartout
 //
-//  Created by Davide De Rosa on 12/19/23.
+//  Created by Davide De Rosa on 2/3/25.
 //  Copyright (c) 2025 Davide De Rosa. All rights reserved.
 //
 //  https://github.com/passepartoutvpn
@@ -23,22 +23,18 @@
 //  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-import Foundation
+import SwiftUI
 
-public enum AppUserLevel: Int, Sendable {
-    case undefined = -1
+struct VerificationView: View {
+    let isVerifying: Bool
 
-    case freemium = 0
-
-    case beta = 1
-
-    case fullV2 = 2 // without .appleTV
-
-    case fullV3 = 3
-}
-
-extension AppUserLevel {
-    public var isRestricted: Bool {
-        self == .beta
+    var body: some View {
+        HStack {
+            Text(Strings.Views.App.Folders.default)
+            if isVerifying {
+                Spacer()
+                Text(Strings.Views.Paywall.Alerts.Verifying.title.withTrailingDots)
+            }
+        }
     }
 }

Packages/App/Sources/AppUIMain/Views/Modules/OpenVPNView.swift
@@ -117,7 +117,7 @@ private extension OpenVPNView {
     }
 
     var importButton: some View {
-        Button(Strings.Modules.General.Rows.importFromFile.withTrailingDots) {
+        Button(Strings.Modules.General.Rows.importFromFile.forMenu) {
             isImporting = true
         }
     }

commit d51c60c5c8659fd748d8af3c134e7c307683be88

Fix "No profiles" message flashing on launch (#1147)
Due to a regression in #1135

Packages/App/Sources/AppUIMain/Views/App/ProfileContainerView.swift
@@ -109,8 +109,9 @@ private struct ContainerModifier: ViewModifier {
     func body(content: Content) -> some View {
         debugChanges()
         return content
-            .themeEmpty(
-                if: !profileManager.hasProfiles,
+            .themeProgress(
+                if: !profileManager.isReady,
+                isEmpty: !profileManager.hasProfiles,
                 emptyContent: emptyView
             )
             .searchable(text: $search)