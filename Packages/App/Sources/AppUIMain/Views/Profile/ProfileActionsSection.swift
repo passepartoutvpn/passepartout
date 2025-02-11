passepartoutvpn/passepartout

commit 36d9ac59b84fbca98f6d9f1d83cd8097ca1d8b4c

macOS: Fix empty space in "Delete profile"
Show nothing when profile is new.

Packages/App/Sources/AppUIMain/Views/Profile/ProfileActionsSection.swift
@@ -49,16 +49,26 @@ struct ProfileActionsSection: View {
                 .frame(maxWidth: .infinity, alignment: .center)
         }
 #else
-        UUIDText(uuid: profileId)
-            .asSectionWithTrailingContent {
-                removeContent
-            }
+        if isExistingProfile {
+            uuidView
+                .asSectionWithTrailingContent(removeContent)
+        } else {
+            uuidView
+        }
 #endif
     }
 }
 
 private extension ProfileActionsSection {
-    var removeContent: some View {
+    var isExistingProfile: Bool {
+        profileManager.profile(withId: profileId) != nil
+    }
+
+    var uuidView: some View {
+        UUIDText(uuid: profileId)
+    }
+
+    func removeContent() -> some View {
         profileManager.profile(withId: profileId)
             .map { _ in
                 removeButton

commit c303e948aedc40c41c6b64ef4654e87cbf2a5aaa

Fix compile error on iOS
Caused by 36d9ac59b84fbca98f6d9f1d83cd8097ca1d8b4c

Packages/App/Sources/AppUIMain/Views/Profile/ProfileActionsSection.swift
@@ -45,7 +45,7 @@ struct ProfileActionsSection: View {
             UUIDText(uuid: profileId)
         }
         Section {
-            removeContent
+            removeContent()
                 .frame(maxWidth: .infinity, alignment: .center)
         }
 #else