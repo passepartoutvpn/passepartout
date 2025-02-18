42a186102922b0a3f5a16836533ba01d35d7c703
Davide <keeshux@gmail.com>
2025-02-18 01:16:08 +0100

    Apply system appearance via UIKit/AppKit (#1200)
    SwiftUI implementation is fragile, system appearance with
    `preferredColorScheme(nil)` is buggy.
    
    Instead, rely on:
    
    - iOS: UIKit → UIWindow.overrideUserInterfaceStyle
    - macOS: AppKit → NSApp.appearance
    
    Amends #1077 
    
    Fixes #1199

---
+++ Packages/App/Sources/UILibrary/Business/AppearanceManager.swift
@@ -0,0 +1,76 @@
+//
+//  AppearanceManager.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 2/18/25.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+public final class AppearanceManager: ObservableObject {
+    private let defaults: UserDefaults
+
+    @Published
+    public var systemAppearance: SystemAppearance? {
+        didSet {
+            defaults.set(systemAppearance?.rawValue, forKey: UIPreference.systemAppearance.key)
+            apply()
+        }
+    }
+
+    public init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+        systemAppearance = defaults.string(forKey: UIPreference.systemAppearance.key)
+            .flatMap {
+                SystemAppearance(rawValue: $0)
+            }
+    }
+}
+
+private extension AppearanceManager {
+    func apply() {
+#if os(iOS)
+        guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = scene.keyWindow else {
+            return
+        }
+        switch systemAppearance {
+        case .light:
+            window.overrideUserInterfaceStyle = .light
+        case .dark:
+            window.overrideUserInterfaceStyle = .dark
+        case .none:
+            window.overrideUserInterfaceStyle = .unspecified
+        }
+#elseif os(macOS)
+        switch systemAppearance {
+        case .light:
+            NSApp.appearance = NSAppearance(named: .vibrantLight)
+        case .dark:
+            NSApp.appearance = NSAppearance(named: .vibrantDark)
+        case .none:
+            NSApp.appearance = nil
+        }
+#endif
+    }
+}
