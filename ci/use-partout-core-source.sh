f86687f261aec674bcaf90ad2aeb47c7ad34b0cf
Davide <keeshux@gmail.com>
2025-04-21 13:31:11 +0200

    Sed and grep to test Core env/SHA-1

--- ci/use-partout-core-source.sh
+++ ci/use-partout-core-source.sh
@@ -2,9 +2,21 @@
 manifest="Submodules/partout/Package.swift"
 core_status=`git submodule status Submodules/partout-core`
 sha1=${core_status:1:40}
-echo "Update partout manifest, partout-core -> $sha1"
 
-sha1_line="let sha1 = .*"
-env_line="environment = .remoteBinary"
-sed -i '' "s/^${sha1_line}$/let sha1 = \"${sha1}\"/" "$manifest"
-sed -i '' "s/^${env_line}$/environment = .remoteSource/" "$manifest"
+env_line_old="environment = .remoteBinary"
+env_line_new="environment = .remoteSource"
+sed -i '' "s/^${env_line_old}$/${env_line_new}/" "$manifest"
+if ! grep -q "^${env_line_new}$" "$manifest"; then
+    echo "Unable to set environment"
+    exit 1
+fi
+
+sha1_line_pattern="let sha1 = .*"
+sha1_line_new="let sha1 = \"${sha1}\""
+sed -i '' "s/^${sha1_line_pattern}$/${sha1_line_new}/" "$manifest"
+if ! grep -q "^${sha1_line_new}$" "$manifest"; then
+    echo "Unable to set SHA-1"
+    exit 1
+fi
+
+echo "Updated partout manifest, partout-core -> $sha1"
