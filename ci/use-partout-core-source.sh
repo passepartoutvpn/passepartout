3bf081019d74d376f1c77078bf738e6e643249c0
Davide <keeshux@gmail.com>
2025-04-21 12:42:55 +0200

    Ensure that PartoutCore source matches submodule

--- ci/use-partout-core-source.sh
+++ ci/use-partout-core-source.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+manifest="Submodules/partout/Package.swift"
+sha1=`git submodule status Submodules/partout-core | cut -d ' ' -f 2`
+sha1_line="let sha1 = .*"
 env_line="environment = .remoteBinary"
-sed -i '' "s/^${env_line}$/environment = .remoteSource/" "Submodules/partout/Package.swift"
+sed -i '' "s/^${sha1_line}$/let sha1 = \"${sha1}\"/" "$manifest"
+sed -i '' "s/^${env_line}$/environment = .remoteSource/" "$manifest"
