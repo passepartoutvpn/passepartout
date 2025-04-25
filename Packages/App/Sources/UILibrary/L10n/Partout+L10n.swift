146a8b9e3975dbf5880b4ea9a3957c2d78e3a9f0
Davide <keeshux@gmail.com>
2025-04-25 11:36:20 +0200

    Opt for a more readable solution

--- Packages/App/Sources/UILibrary/L10n/Partout+L10n.swift
+++ Packages/App/Sources/UILibrary/L10n/Partout+L10n.swift
@@ -60,7 +60,7 @@ extension Profile: StyledOptionalLocalizableEntity {
                 .first {
                     primaryCondition(for: $0)
                 }?
-                .moduleType
+                .effectiveModuleType
                 .localizedDescription
 
         case .secondaryTypes:
@@ -77,6 +77,15 @@ extension Profile: StyledOptionalLocalizableEntity {
 
     private func primaryCondition(for module: Module) -> Bool {
         module is ProviderModule || module.buildsConnection
+    }
+}
+
+private extension Module {
+    var effectiveModuleType: ModuleType {
+        if let providerModule = self as? ProviderModule {
+            return providerModule.providerModuleType
+        }
+        return moduleType
     }
 }
 
