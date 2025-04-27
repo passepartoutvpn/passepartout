passepartoutvpn/passepartout

commit e3bfc65230bea3c0e60aa9acf7417c807d431f23

Require a comment before submitting an issue (#1342)
Fixes #1315

Packages/App/Sources/UILibrary/Views/Theme/ThemeTextInputView.swift
@@ -1,0 +1,94 @@
+//
+//  ThemeTextInputView.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 4/25/25.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+
+public struct ThemeTextInputView: View {
+    private let title: String
+
+    @Binding
+    private var isPresented: Bool
+
+    private let onValidate: ((String) -> Bool)?
+
+    private let onSubmit: (String) -> Void
+
+    @State
+    private var text = ""
+
+    public init(
+        _ title: String,
+        isPresented: Binding<Bool>,
+        onValidate: ((String) -> Bool)? = nil,
+        onSubmit: @escaping (String) -> Void
+    ) {
+        self.title = title
+        _isPresented = isPresented
+        self.onValidate = onValidate
+        self.onSubmit = onSubmit
+    }
+
+    public var body: some View {
+        TextEditor(text: $text)
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button(Strings.Global.Nouns.ok) {
+                        isPresented = false
+                        onSubmit(text)
+                    }
+                    .disabled(onValidate?(text) == false)
+                }
+            }
+            .themeForm()
+            .navigationTitle(title)
+            .themeNavigationDetail()
+            .themeNavigationStack(closable: true)
+    }
+}
+
+#Preview {
+    struct Preview: View {
+        @State
+        private var isPresented = false
+
+        var body: some View {
+            Button("Present") {
+                isPresented = true
+            }
+            .sheet(isPresented: $isPresented) {
+                ThemeTextInputView(
+                    "Comment",
+                    isPresented: $isPresented,
+                    onValidate: { $0.count >= 10 },
+                    onSubmit: { _ in }
+                )
+            }
+        }
+    }
+
+    return Preview()
+        .withMockEnvironment()
+}

Packages/App/Sources/AppUIMain/Domain/Issue+Metadata.swift
@@ -43,6 +43,8 @@ extension Issue {
         let urlForTunnelLog: URL
 
         let parameters: Constants.Log
+
+        let comment: String
     }
 
     @MainActor
@@ -71,6 +73,7 @@ extension Issue {
         }
 
         return Issue(
+            comment: metadata.comment,
             appLine: "\(Strings.Unlocalized.appName) \(metadata.versionString)",
             purchasedProducts: metadata.purchasedProducts,
             appLog: appLog,

Packages/App/Sources/AppUIMain/Domain/Issue.swift
@@ -29,6 +29,8 @@ import Foundation
 struct Issue: Identifiable {
     let id: UUID
 
+    let comment: String
+
     let appLine: String?
 
     let purchasedProducts: Set<AppProduct>
@@ -46,6 +48,7 @@ struct Issue: Identifiable {
     let providerLastUpdate: Date?
 
     init(
+        comment: String,
         appLine: String?,
         purchasedProducts: Set<AppProduct>,
         appLog: Data? = nil,
@@ -53,6 +56,7 @@ struct Issue: Identifiable {
         provider: (ProviderID, Date?)? = nil
     ) {
         id = UUID()
+        self.comment = comment
         self.appLine = appLine
         self.purchasedProducts = purchasedProducts
         self.appLog = appLog
@@ -68,6 +72,7 @@ struct Issue: Identifiable {
 
     var body: String {
         template
+            .replacingOccurrences(of: "$comment", with: comment)
             .replacingOccurrences(of: "$appLine", with: appLine ?? "unknown")
             .replacingOccurrences(of: "$osLine", with: osLine)
             .replacingOccurrences(of: "$deviceLine", with: deviceLine ?? "unknown")

Packages/App/Sources/AppUIMain/Views/Diagnostics/DiagnosticsView.swift
@@ -25,6 +25,7 @@
 
 import CommonLibrary
 import SwiftUI
+import UIAccessibility
 
 struct DiagnosticsView: View {
     struct LogEntry: Identifiable, Equatable {
@@ -82,7 +83,7 @@ struct DiagnosticsView: View {
             liveLogSection
             openVPNSection
             tunnelLogsSection
-            if iapManager.isEligibleForFeedback {
+            if AppCommandLine.contains(.withReportIssue) || iapManager.isEligibleForFeedback {
                 reportIssueSection
             }
         }

Packages/App/Sources/AppUIMain/Views/Diagnostics/ReportIssueButton.swift
@@ -47,7 +47,7 @@ struct ReportIssueButton {
     var isPending = false
 
     @State
-    var issueBeingReported: Issue?
+    var modalRoute: ModalRoute?
 
     var installedProfile: Profile? {
         guard let id = tunnel.currentProfile?.id else {
@@ -62,5 +62,46 @@ struct ReportIssueButton {
         }
         let lastUpdate = apiManager.cache(for: providerId)?.lastUpdate
         return (providerId, lastUpdate)
+    }
+}
+
+extension ReportIssueButton {
+    enum ModalRoute: Identifiable {
+        case comment
+
+        case submit(Issue)
+
+        var id: Int {
+            switch self {
+            case .comment: return 1
+            case .submit: return 2
+            }
+        }
+    }
+}
+
+extension ReportIssueButton {
+    func commentInputView() -> some View {
+        ThemeTextInputView(
+            Strings.Global.Nouns.comment,
+            isPresented: Binding {
+                switch modalRoute {
+                case .comment:
+                    return true
+                default:
+                    return false
+                }
+            } set: {
+                if !$0 {
+                    modalRoute = nil
+                }
+            },
+            onValidate: {
+                !$0.isEmpty
+            },
+            onSubmit: {
+                sendEmail(comment: $0)
+            }
+        )
     }
 }

Packages/App/Sources/AppUIMain/Views/Diagnostics/iOS/ReportIssueButton+iOS.swift
@@ -33,35 +33,50 @@ import UIKit
 extension ReportIssueButton: View {
     var body: some View {
         HStack {
-            Button(title, action: sendEmail)
+            Button(title) {
+                modalRoute = .comment
+            }
             if isPending {
                 Spacer()
                 ProgressView()
             }
         }
         .disabled(isPending)
-        .themeModal(item: $issueBeingReported) {
-            MailComposerView(
-                isPresented: Binding {
-                    issueBeingReported != nil
-                } set: {
-                    if !$0 {
-                        issueBeingReported = nil
-                    }
-                },
-                toRecipients: [$0.to],
-                subject: $0.subject,
-                messageBody: $0.body,
-                attachments: $0.attachments
-            )
+        .themeModal(item: $modalRoute) {
+            switch $0 {
+            case .comment:
+                commentInputView()
+            case .submit(let issue):
+                emailComposerView(issue: issue)
+            }
         }
     }
 }
 
-private extension ReportIssueButton {
+extension ReportIssueButton {
+    func emailComposerView(issue: Issue) -> some View {
+        MailComposerView(
+            isPresented: Binding {
+                switch modalRoute {
+                case .submit:
+                    return true
+                default:
+                    return false
+                }
+            } set: {
+                if !$0 {
+                    modalRoute = nil
+                }
+            },
+            toRecipients: [issue.to],
+            subject: issue.subject,
+            messageBody: issue.body,
+            attachments: issue.attachments
+        )
+    }
 
     @MainActor
-    func sendEmail() {
+    func sendEmail(comment: String) {
         Task {
             isPending = true
             defer {
@@ -75,13 +90,14 @@ private extension ReportIssueButton {
                 purchasedProducts: purchasedProducts,
                 tunnel: tunnel,
                 urlForTunnelLog: BundleConfiguration.urlForTunnelLog,
-                parameters: Constants.shared.log
+                parameters: Constants.shared.log,
+                comment: comment
             ))
             guard MailComposerView.canSendMail() else {
                 openMailTo(with: issue)
                 return
             }
-            issueBeingReported = issue
+            modalRoute = .submit(issue)
         }
     }
 

Packages/App/Sources/AppUIMain/Views/Diagnostics/macOS/ReportIssueButton+macOS.swift
@@ -30,15 +30,24 @@ import SwiftUI
 
 extension ReportIssueButton: View {
     var body: some View {
-        Button(title, action: sendEmail)
-            .disabled(isPending)
+        Button(title) {
+            modalRoute = .comment
+        }
+        .disabled(isPending)
+        .themeModal(
+            item: $modalRoute,
+            options: ThemeModalOptions(size: .small),
+            content: { _ in
+                commentInputView()
+            }
+        )
     }
 }
 
-private extension ReportIssueButton {
+extension ReportIssueButton {
 
     @MainActor
-    func sendEmail() {
+    func sendEmail(comment: String) {
         Task {
             isPending = true
             defer {
@@ -56,7 +65,8 @@ private extension ReportIssueButton {
                 purchasedProducts: purchasedProducts,
                 tunnel: tunnel,
                 urlForTunnelLog: BundleConfiguration.urlForTunnelLog,
-                parameters: Constants.shared.log
+                parameters: Constants.shared.log,
+                comment: comment
             ))
             service.recipients = [issue.to]
             service.subject = issue.subject

Packages/App/Sources/UIAccessibility/IPC/AppCommandLine.swift
@@ -31,6 +31,8 @@ public enum AppCommandLine {
 
         case fakeMigration = "-pp_fake_migration"
 
+        case withReportIssue = "-pp_with_report_issue"
+
         case uiTesting = "-pp_ui_testing"
     }
 

commit be9f8f5cb12901574971db82ddacfdbc71bb6f4e

Fix tvOS compile error on TextEditor
Caused by #1342 (e3bfc65230bea3c0e60aa9acf7417c807d431f23)

Packages/App/Sources/UILibrary/Views/Theme/ThemeTextInputView.swift
@@ -23,6 +23,8 @@
 //  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+#if !os(tvOS)
+
 import SwiftUI
 
 public struct ThemeTextInputView: View {
@@ -92,3 +94,5 @@ public struct ThemeTextInputView: View {
     return Preview()
         .withMockEnvironment()
 }
+
+#endif