23d0a162f6d9b35858fba127e1484a127b17da8b
Davide <keeshux@gmail.com>
2025-05-01 17:32:04 +0200

    Drop codesign from embed script

--- Passepartout/Scripts/embed-extensions.sh
+++ Passepartout/Scripts/embed-extensions.sh
@@ -3,7 +3,6 @@ app_root="${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app"
 login_src="${BUILT_PRODUCTS_DIR}/PassepartoutLoginItem.app"
 appex_src="${BUILT_PRODUCTS_DIR}/PassepartoutTunnel.appex"
 sysex_src="${BUILT_PRODUCTS_DIR}/com.algoritmico.mac.Passepartout.Tunnel.systemextension"
-#codesign_identity="${EXPANDED_CODE_SIGN_IDENTITY}"
 cp_alias="cp -rP"
 
 if [ "$PLATFORM_NAME" == "macosx" ]; then
@@ -13,14 +12,12 @@ if [ "$PLATFORM_NAME" == "macosx" ]; then
 fi
 
 if [ "$CONFIGURATION" == "ReleaseMac" ]; then
-    #codesign --force --sign "$codesign_identity" "$sysex_src" --timestamp=none
     sysex_dst="${app_root}/Contents/Library/SystemExtensions"
     mkdir "$sysex_dst"
     $cp_alias "$sysex_src" "$sysex_dst"
     exit
 fi
 
-#codesign --force --sign "$codesign_identity" "$appex_src" --timestamp=none
 if [ "$PLATFORM_NAME" == "macosx" ]; then
     appex_dst="${app_root}/Contents/PlugIns"
 else
