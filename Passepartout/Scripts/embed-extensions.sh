8c74e3ebbaca059577172371f1205a4d0eb073e3
Davide <keeshux@gmail.com>
2025-05-01 17:48:53 +0200

    Use more variables in script

--- Passepartout/Scripts/embed-extensions.sh
+++ Passepartout/Scripts/embed-extensions.sh
@@ -3,23 +3,26 @@ app_root="${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app"
 login_src="${BUILT_PRODUCTS_DIR}/PassepartoutLoginItem.app"
 appex_src="${BUILT_PRODUCTS_DIR}/PassepartoutTunnel.appex"
 sysex_src="${BUILT_PRODUCTS_DIR}/com.algoritmico.mac.Passepartout.Tunnel.systemextension"
+platform_mac="macosx"
+sysex_cfg="ReleaseMac"
+
 mkdir_alias="mkdir -p"
 cp_alias="cp -rP"
 
-if [ "$PLATFORM_NAME" == "macosx" ]; then
+if [ "$PLATFORM_NAME" == "$platform_mac" ]; then
     login_dst="${app_root}/Contents/Library/LoginItems"
     $mkdir_alias "$login_dst"
     $cp_alias "$login_src" "$login_dst"
 fi
 
-if [ "$CONFIGURATION" == "ReleaseMac" ]; then
+if [ "$CONFIGURATION" == "$sysex_cfg" ]; then
     sysex_dst="${app_root}/Contents/Library/SystemExtensions"
     $mkdir_alias "$sysex_dst"
     $cp_alias "$sysex_src" "$sysex_dst"
     exit
 fi
 
-if [ "$PLATFORM_NAME" == "macosx" ]; then
+if [ "$PLATFORM_NAME" == "$platform_mac" ]; then
     appex_dst="${app_root}/Contents/PlugIns"
 else
     appex_dst="${app_root}/PlugIns"
