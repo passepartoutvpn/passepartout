5563fb31d3ddc0d23c1076254c26b47cbfd178ca
Davide <keeshux@gmail.com>
2025-05-01 17:37:19 +0200

    Fix dir without parent in embed script

--- Passepartout/Scripts/embed-extensions.sh
+++ Passepartout/Scripts/embed-extensions.sh
@@ -3,17 +3,18 @@ app_root="${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app"
 login_src="${BUILT_PRODUCTS_DIR}/PassepartoutLoginItem.app"
 appex_src="${BUILT_PRODUCTS_DIR}/PassepartoutTunnel.appex"
 sysex_src="${BUILT_PRODUCTS_DIR}/com.algoritmico.mac.Passepartout.Tunnel.systemextension"
+mkdir_alias="mkdir -p"
 cp_alias="cp -rP"
 
 if [ "$PLATFORM_NAME" == "macosx" ]; then
     login_dst="${app_root}/Contents/Library/LoginItems"
-    mkdir "$login_dst"
+    $mkdir_alias "$login_dst"
     $cp_alias "$login_src" "$login_dst"
 fi
 
 if [ "$CONFIGURATION" == "ReleaseMac" ]; then
     sysex_dst="${app_root}/Contents/Library/SystemExtensions"
-    mkdir "$sysex_dst"
+    $mkdir_alias "$sysex_dst"
     $cp_alias "$sysex_src" "$sysex_dst"
     exit
 fi
@@ -23,5 +24,5 @@ if [ "$PLATFORM_NAME" == "macosx" ]; then
 else
     appex_dst="${app_root}/PlugIns"
 fi
-mkdir "$appex_dst"
+$mkdir_alias "$appex_dst"
 $cp_alias "$appex_src" "$appex_dst"
