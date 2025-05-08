c1059d70f9deefb7e08b877e3e04bd65e2e2e258
Davide <keeshux@gmail.com>
2025-05-08 17:06:00 +0200

    Use as local env
    Not from GitHub.

--- scripts/export-certs.sh
+++ scripts/export-certs.sh
@@ -5,7 +5,7 @@ source $cwd/env.secrets.sh
 cd $cwd/..
 
 set -e
-if [[ -z $CERTIFICATES_PASSPHRASE ]]; then
+if [[ -z $certs_passphrase ]]; then
     echo "Passphrase required"
     exit 1
 fi
@@ -13,4 +13,4 @@ rm -rf $tmp_root
 mkdir $tmp_root
 cp "$certs_p12_path" "$certs_profiles_root"/*.mobileprovision "$certs_profiles_root"/*.provisionprofile "$tmp_root"
 rm -f "$certs_zip_path"
-zip -ejr9 -P "$CERTIFICATES_PASSPHRASE" "$certs_zip_path" "$tmp_root"
+zip -ejr9 -P "$certs_passphrase" "$certs_zip_path" "$tmp_root"
