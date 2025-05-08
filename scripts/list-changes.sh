205567e0156492bc84018de86d4a78813e676a06
Davide <keeshux@gmail.com>
2025-05-08 18:10:09 +0200

    List changes from 3 repositories

---
+++ scripts/list-changes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ref_from=$1
+ref_to=$2
+if [[ -z $ref_from ]]; then
+    echo "Initial ref required"
+    exit 1
+fi
+
+old_modules=(`git diff "${ref_from}".."${ref_to}" submodules/ | grep -- "-Subproject" | cut -d ' ' -f 3 | tr "\n" " "`)
+new_modules=(`git diff "${ref_from}".."${ref_to}" submodules/ | grep -- "\+Subproject" | cut -d ' ' -f 3 | tr "\n" " "`)
+partout_range="${old_modules[0]}..${new_modules[0]}"
+partout_core_range="${old_modules[1]}..${new_modules[1]}"
+
+function git_cmd() {
+    local from=$1
+    local to=$2
+    if [[ -z $from || -z $to ]]; then
+        return
+    fi
+    name=$(basename $(pwd))
+    echo "=== $name ==="
+    echo ""
+    git --no-pager log ${from}..${to} --oneline
+    echo ""
+}
+
+git_cmd $ref_from $ref_to
+(cd submodules/partout && git_cmd ${old_modules[0]} ${new_modules[0]})
+(cd submodules/partout-core && git_cmd ${old_modules[1]} ${new_modules[1]})
