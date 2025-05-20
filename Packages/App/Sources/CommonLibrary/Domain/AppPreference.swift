6b2e48b418b58019cfeba2c299faf4eba0ed8d25
Davide <keeshux@gmail.com>
2025-05-20 18:44:18 +0200

    Fix default value for DNS fallback (#1378)
    Currently false, must default to true. Also, add missing fallback logic
    to shortcut methods of KeyValueManager.
    
    Regression in #1376

--- Packages/App/Sources/CommonLibrary/Domain/AppPreference.swift
+++ Packages/App/Sources/CommonLibrary/Domain/AppPreference.swift
@@ -43,7 +43,7 @@ public enum AppPreference: String, PreferenceProtocol {
 }
 
 public struct AppPreferenceValues: Sendable {
-    public var dnsFallsBack = false
+    public var dnsFallsBack = true
 
     public var skipsPurchases = false
 
