7ffe37ea0d9e280b7cafbeff1332d431173be2d4
Davide <keeshux@gmail.com>
2025-05-23 19:37:29 +0200

    Include .dmg in GitHub Release (#1391)
    Download the artifacts from the .dmg job to attach them to the GitHub
    Release.
    
    Additionally:
    
    - Drop (supposedly) unused env variables
    - Tag release without fastlane
    
    Fixes #1387

--- ci/gpg-import.sh
+++ ci/gpg-import.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-key_file="$1"
-if [[ -z "$key_file" ]]; then
+key_content="$1"
+if [[ -z "$key_content" ]]; then
     echo "Missing key file"
     exit 1
 fi
+key_file="gpg.key"
+echo "$key_content" >"$key_file"
 gpg --batch --yes --pinentry-mode loopback --import "$key_file"
+rm "$key_file"
