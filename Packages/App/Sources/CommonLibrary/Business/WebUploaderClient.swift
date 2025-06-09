459eee64f81273140d51b1dfcffdbc21d8111595
Davide <keeshux@gmail.com>
2025-06-09 18:15:06 +0200

    Add WebUploaderClient with HTTP upload logic

---
+++ Packages/App/Sources/CommonLibrary/Business/WebUploaderClient.swift
@@ -0,0 +1,71 @@
+//
+//  WebUploaderClient.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 6/9/25.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import CommonUtils
+import Foundation
+
+public final class WebUploaderClient: ObservableObject, Sendable {
+    private let registryCoder: RegistryCoder
+
+    private let profile: Profile
+
+    public init(registryCoder: RegistryCoder, profile: Profile) {
+        self.registryCoder = registryCoder
+        self.profile = profile
+    }
+
+    public func send(to url: URL, passcode: String) async throws {
+        pp_log_g(.app, .info, "WebUploaderClient: sending to \(url) with passcode \(passcode)")
+        let encodedProfile = try registryCoder.string(from: profile)
+
+        var formBuilder = MultipartForm.Builder()
+        formBuilder.fields["passcode"] = MultipartForm.Field(passcode)
+        formBuilder.fields["file"] = MultipartForm.Field(encodedProfile, filename: profile.name)
+        let form = formBuilder.build()
+        let request = form.toURLRequest(url: url)
+
+        do {
+            let (_, response) = try await URLSession.shared.data(for: request)
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw AppError.webUploader()
+            }
+            guard httpResponse.statusCode == 200 else {
+                switch httpResponse.statusCode {
+                case 400:
+                    assertionFailure("WebUploaderClient: invalid form, bug in MultipartForm")
+                case 403:
+                    assertionFailure("WebUploaderClient: passcode is missing or incorrect")
+                case 404:
+                    assertionFailure("WebUploaderClient: URL not found")
+                default:
+                    break
+                }
+                throw AppError.webUploader()
+            }
+        } catch {
+            throw AppError.webUploader(error)
+        }
+    }
+}
