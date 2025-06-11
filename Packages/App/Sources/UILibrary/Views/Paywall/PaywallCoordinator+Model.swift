passepartoutvpn/passepartout

commit d8d25ea6382cb8ba87b860054e87e68a2ecabd3f

Extend paywall with individual purchases (#1422)
We want to offer a "Purchase" button in the purchase confirmation alert,
in addition to the "Connect" and "Save anyway" fallback actions. This is
problematic in case the profile requires the purchase of more than 1
product, because StoreKit only allows 1 purchase at a time.

Therefore, split the paywall into two sections:

- Complete packages (if eligible)
- Individual purchases

In the case of individual purchases, let the user purchase them one at a
time. Gray out the purchased ones, and dismiss the paywall after
purchasing all of them.

The PR prepares for the reintroduction of the paywall on tvOS, but this
will be done later (it crashes now). For now, share the paywall view
logic in PaywallCoordinator and split the iOS/macOS (+Main) and tvOS
(+TV) layouts.

Packages/App/Sources/UILibrary/Views/Paywall/PaywallCoordinator+Model.swift
@@ -1,0 +1,141 @@
+//
+//  PaywallCoordinator+Model.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 6/12/25.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import CommonIAP
+import CommonLibrary
+import CommonUtils
+import Foundation
+
+extension PaywallCoordinator {
+
+    @MainActor
+    final class Model: ObservableObject {
+        var isFetchingProducts = true
+
+        private(set) var suggestedProducts: Set<AppProduct> = []
+
+        private(set) var completePurchasable: [InAppProduct] = []
+
+        private(set) var individualPurchasable: [InAppProduct] = []
+
+        var purchasingIdentifier: String?
+
+        var isPurchasePendingConfirmation = false
+    }
+}
+
+extension PaywallCoordinator.Model {
+    func fetchAvailableProducts(
+        for requiredFeatures: Set<AppFeature>,
+        with iapManager: IAPManager
+    ) async throws {
+        isFetchingProducts = true
+        defer {
+            isFetchingProducts = false
+        }
+        do {
+            let rawProducts = iapManager.suggestedProducts(for: requiredFeatures)
+            guard !rawProducts.isEmpty else {
+                throw AppError.emptyProducts
+            }
+            let rawSortedProducts = rawProducts.sorted {
+                $0.productRank < $1.productRank
+            }
+            let purchasable = try await iapManager.purchasableProducts(for: rawSortedProducts)
+            try setSuggestedProducts(rawProducts, purchasable: purchasable)
+        } catch {
+            pp_log_g(.App.iap, .error, "Unable to load purchasable products: \(error)")
+            throw error
+        }
+    }
+
+    func setSuggestedProducts(
+        _ suggestedProducts: Set<AppProduct>,
+        purchasable: [InAppProduct]
+    ) throws {
+        let completeProducts = suggestedProducts.filter(\.isComplete)
+
+        var completePurchasable: [InAppProduct] = []
+        var individualPurchasable: [InAppProduct] = []
+        purchasable.forEach {
+            guard let raw = AppProduct(rawValue: $0.productIdentifier) else {
+                return
+            }
+            if completeProducts.contains(raw) {
+                completePurchasable.append($0)
+            } else {
+                individualPurchasable.append($0)
+            }
+        }
+        pp_log_g(.App.iap, .info, "Individual products: \(individualPurchasable)")
+        guard !completePurchasable.isEmpty || !individualPurchasable.isEmpty else {
+            throw AppError.emptyProducts
+        }
+
+        self.suggestedProducts = suggestedProducts
+        self.completePurchasable = completePurchasable
+        self.individualPurchasable = individualPurchasable
+    }
+}
+
+private extension AppProduct {
+    var productRank: Int {
+        switch self {
+        case .Essentials.iOS_macOS:
+            return .min
+        case .Essentials.iOS:
+            return 1
+        case .Essentials.macOS:
+            return 2
+        case .Complete.Recurring.yearly:
+            return 3
+        case .Complete.Recurring.monthly:
+            return 4
+        default:
+            return .max
+        }
+    }
+}
+
+extension PaywallCoordinator.Model {
+
+    @MainActor
+    static func forPreviews(
+        _ features: Set<AppFeature>,
+        including: Set<IAPManager.SuggestionInclusion>
+    ) -> PaywallCoordinator.Model {
+        let state = PaywallCoordinator.Model()
+        state.isFetchingProducts = false
+        let suggested = IAPManager.forPreviews.suggestedProducts(
+            for: features,
+            including: including
+        )
+        try? state.setSuggestedProducts(
+            suggested,
+            purchasable: suggested.map(\.asFakeIAP)
+        )
+        return state
+    }
+}

Packages/App/Sources/UILibrary/Views/Paywall/PaywallCoordinator.swift
@@ -1,0 +1,148 @@
+//
+//  PaywallCoordinator.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 9/10/24.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import CommonIAP
+import CommonLibrary
+import CommonUtils
+import SwiftUI
+
+struct PaywallCoordinator: View {
+
+    @EnvironmentObject
+    private var iapManager: IAPManager
+
+    @Binding
+    var isPresented: Bool
+
+    let requiredFeatures: Set<AppFeature>
+
+    @State
+    private var model = Model()
+
+    @StateObject
+    private var errorHandler: ErrorHandler = .default()
+
+    var body: some View {
+        contentView
+            .themeProgress(if: model.isFetchingProducts)
+            .disabled(model.purchasingIdentifier != nil)
+            .alert(
+                Strings.Global.Actions.purchase,
+                isPresented: $model.isPurchasePendingConfirmation,
+                actions: pendingActions,
+                message: pendingMessage
+            )
+            .task(id: requiredFeatures) {
+                do {
+                    try await model.fetchAvailableProducts(
+                        for: requiredFeatures,
+                        with: iapManager
+                    )
+                } catch {
+                    onError(error, dismissing: true)
+                }
+            }
+            .withErrorHandler(errorHandler)
+    }
+}
+
+private extension PaywallCoordinator {
+    var contentView: some View {
+#if os(tvOS)
+        PaywallView(
+            requiredFeatures: requiredFeatures,
+            state: state
+        )
+        .themeNavigationStack()
+#else
+        PaywallView(
+            isPresented: $isPresented,
+            iapManager: iapManager,
+            requiredFeatures: requiredFeatures,
+            model: $model,
+            errorHandler: errorHandler,
+            onComplete: onComplete,
+            onError: onError
+        )
+        .themeNavigationStack(closable: true)
+#endif
+    }
+
+    func pendingActions() -> some View {
+        Button(Strings.Global.Nouns.ok) {
+            isPresented = false
+        }
+    }
+
+    func pendingMessage() -> some View {
+        Text(Strings.Views.Paywall.Alerts.Pending.message)
+    }
+}
+
+private extension PaywallCoordinator {
+    var didPurchaseRequired: Bool {
+        iapManager.didPurchaseComplete || iapManager.didPurchase(model.individualPurchasable)
+    }
+
+    func onComplete(_ productIdentifier: String, result: InAppPurchaseResult) {
+        switch result {
+        case .done:
+            Task {
+                await iapManager.reloadReceipt()
+                if didPurchaseRequired {
+                    isPresented = false
+                }
+            }
+        case .pending:
+            model.isPurchasePendingConfirmation = true
+        case .cancelled:
+            break
+        case .notFound:
+            fatalError("Product not found: \(productIdentifier)")
+        }
+    }
+
+    func onError(_ error: Error) {
+        onError(error, dismissing: false)
+    }
+
+    func onError(_ error: Error, dismissing: Bool) {
+        errorHandler.handle(error, title: Strings.Global.Actions.purchase) {
+            if dismissing {
+                isPresented = false
+            }
+        }
+    }
+}
+
+// MARK: - Previews
+
+#Preview {
+    PaywallCoordinator(
+        isPresented: .constant(true),
+        requiredFeatures: [.appleTV, .dns, .sharing]
+    )
+    .withMockEnvironment()
+}

Packages/App/Sources/UILibrary/Views/Paywall/PaywallView+Main.swift
@@ -1,0 +1,154 @@
+//
+//  PaywallView+Main.swift
+//  Passepartout
+//
+//  Created by Davide De Rosa on 9/10/24.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Passepartout.
+//
+//  Passepartout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Passepartout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#if !os(tvOS)
+
+import CommonLibrary
+import CommonUtils
+import StoreKit
+import SwiftUI
+
+struct PaywallView: View {
+
+    @Binding
+    var isPresented: Bool
+
+    @ObservedObject
+    var iapManager: IAPManager
+
+    let requiredFeatures: Set<AppFeature>
+
+    @Binding
+    var model: PaywallCoordinator.Model
+
+    @ObservedObject
+    var errorHandler: ErrorHandler
+
+    let onComplete: (String, InAppPurchaseResult) -> Void
+
+    let onError: (Error) -> Void
+
+    var body: some View {
+        Form {
+            completeProductsView
+                .if(!model.completePurchasable.isEmpty)
+            individualProductsView
+                .if(!model.individualPurchasable.isEmpty)
+            restoreView
+            linksView
+        }
+        .themeForm()
+    }
+}
+
+private extension PaywallView {
+    var completeProductsView: some View {
+        Group {
+            ForEach(model.completePurchasable, id: \.productIdentifier) {
+                PaywallProductView(
+                    iapManager: iapManager,
+                    style: .paywall,
+                    product: $0,
+                    withIncludedFeatures: false,
+                    highlightedFeatures: requiredFeatures,
+                    purchasingIdentifier: $model.purchasingIdentifier,
+                    onComplete: onComplete,
+                    onError: onError
+                )
+            }
+            VStack(alignment: .leading) {
+                ForEach(AppFeature.allCases.sorted()) {
+                    IncludedFeatureRow(
+                        feature: $0,
+                        isHighlighted: requiredFeatures.contains($0)
+                    )
+                    .font(.subheadline)
+                }
+            }
+        }
+        .themeSection(
+            header: Strings.Views.Paywall.Sections.FullProducts.header,
+            footer: [
+                Strings.Views.Paywall.Sections.FullProducts.footer,
+                Strings.Views.Paywall.Sections.Products.footer
+            ].joined(separator: " "),
+            forcesFooter: true
+        )
+    }
+
+    var individualProductsView: some View {
+        ForEach(model.individualPurchasable, id: \.productIdentifier) {
+            PaywallProductView(
+                iapManager: iapManager,
+                style: .paywall,
+                product: $0,
+                withIncludedFeatures: true,
+                highlightedFeatures: requiredFeatures,
+                purchasingIdentifier: $model.purchasingIdentifier,
+                onComplete: onComplete,
+                onError: onError
+            )
+        }
+        .themeSection(
+            header: Strings.Views.Paywall.Sections.Products.header,
+            footer: Strings.Views.Paywall.Sections.Products.footer,
+            forcesFooter: true
+        )
+    }
+
+    var linksView: some View {
+        Section {
+            Link(Strings.Unlocalized.eula, destination: Constants.shared.websites.eula)
+            Link(Strings.Views.Settings.Links.Rows.privacyPolicy, destination: Constants.shared.websites.privacyPolicy)
+        }
+    }
+
+    var restoreView: some View {
+        RestorePurchasesButton(errorHandler: errorHandler)
+            .themeSectionWithSingleRow(
+                header: Strings.Views.Paywall.Sections.Restore.header,
+                footer: Strings.Views.Paywall.Sections.Restore.footer,
+                above: true
+            )
+    }
+}
+
+// MARK: - Previews
+
+#Preview {
+    let features: Set<AppFeature> = [.appleTV, .dns, .sharing]
+    PaywallView(
+        isPresented: .constant(true),
+        iapManager: .forPreviews,
+        requiredFeatures: features,
+        model: .constant(.forPreviews(features, including: [.complete])),
+        errorHandler: .default(),
+        onComplete: { _, _ in },
+        onError: { _ in }
+    )
+    .withMockEnvironment()
+}
+
+#endif

Packages/App/Sources/AppUIMain/Views/App/AppCoordinator.swift
@@ -60,7 +60,7 @@ public struct AppCoordinator: View, AppCoordinatorConforming, SizeClassProviding
     private var paywallReason: PaywallReason?
 
     @State
-    private var onCancelPaywall: (() -> Void)?
+    private var paywallContinuation: (() -> Void)?
 
     @State
     private var modalRoute: ModalRoute?
@@ -102,14 +102,9 @@ public struct AppCoordinator: View, AppCoordinatorConforming, SizeClassProviding
         ))
         .modifier(PaywallModifier(
             reason: $paywallReason,
-            otherTitle: Strings.Views.Paywall.Alerts.Confirmation.editProfile,
-            onOtherAction: { profile in
-                guard let profile else {
-                    return
-                }
-                onEditProfile(profile.localizedPreview)
-            },
-            onCancel: onCancelPaywall
+            onAction: { _ in
+                paywallContinuation?()
+            }
         ))
         .themeModal(
             item: $modalRoute,
@@ -301,7 +296,7 @@ extension AppCoordinator {
     public func onPurchaseRequired(
         for profile: Profile,
         features: Set<AppFeature>,
-        onCancel: (() -> Void)?
+        continuation: (() -> Void)?
     ) {
         pp_log_g(.app, .info, "Purchase required for features: \(features)")
         guard !iapManager.isLoadingReceipt else {
@@ -315,12 +310,12 @@ extension AppCoordinator {
                     "\n\n",
                     V.Connect._2(iapManager.verificationDelayMinutes)
                 ].joined(separator: " "),
-                onDismiss: onCancel
+                onDismiss: continuation
             )
             return
         }
         pp_log_g(.app, .info, "Present paywall")
-        onCancelPaywall = onCancel
+        paywallContinuation = continuation
 
         setLater(.init(profile, requiredFeatures: features, action: .connect)) {
             paywallReason = $0

Packages/App/Sources/AppUIMain/Views/Profile/ProfileCoordinator.swift
@@ -71,7 +71,12 @@ struct ProfileCoordinator: View {
 
     var body: some View {
         contentView
-            .modifier(PaywallModifier(reason: $paywallReason))
+            .modifier(PaywallModifier(
+                reason: $paywallReason,
+                onAction: { _ in
+                    saveAnyway()
+                }
+            ))
             .themeModal(item: $modalRoute, content: modalDestination)
             .environment(\.dismissProfile, onDismiss)
             .withErrorHandler(errorHandler)
@@ -151,6 +156,12 @@ private extension ProfileCoordinator {
 // MARK: - Actions
 
 private extension ProfileCoordinator {
+    func saveAnyway() {
+        Task {
+            try await commitEditing(verifying: false, dismissing: true)
+        }
+    }
+
     func addNewModule(_ moduleType: ModuleType) {
         let module = moduleType.newModule(with: registry)
         withAnimation(theme.animation(for: .modules)) {
@@ -195,7 +206,6 @@ private extension ProfileCoordinator {
             setLater(PaywallReason(
                 nil,
                 requiredFeatures: requiredFeatures,
-                suggestedProducts: nil,
                 action: .save
             )) {
                 paywallReason = $0

Packages/App/Sources/AppUIMain/Views/Profile/ProfileStorageSection.swift
@@ -73,12 +73,7 @@ private extension ProfileStorageSection {
     var sharingPurchaseButton: some View {
         PurchaseRequiredView(
             requiring: sharingRequirements,
-            reason: $paywallReason,
-            suggesting: {
-                var products = iapManager.suggestedProducts()
-                products.insert(.Features.appleTV)
-                return products
-            }()
+            reason: $paywallReason
         )
     }
 
@@ -96,12 +91,7 @@ private extension ProfileStorageSection {
     var tvPurchaseButton: some View {
         PurchaseRequiredView(
             requiring: tvRequirements,
-            reason: $paywallReason,
-            suggesting: {
-                var products = iapManager.suggestedProducts(filter: .onlyComplete)
-                products.insert(.Features.appleTV)
-                return products
-            }()
+            reason: $paywallReason
         )
     }
 }

Packages/App/Sources/AppUIMain/Views/Profile/macOS/ModuleListView+macOS.swift
@@ -64,8 +64,7 @@ struct ModuleListView: View, Routable {
                         Text(Strings.Global.Nouns.general)
                         PurchaseRequiredView(
                             requiring: requiredGeneralFeatures,
-                            reason: $paywallReason,
-                            suggesting: suggestedGeneralProducts
+                            reason: $paywallReason
                         )
                     }
                 }
@@ -146,12 +145,6 @@ private extension ModuleListView {
             features.insert(.sharing)
         }
         return features
-    }
-
-    var suggestedGeneralProducts: Set<AppProduct> {
-        var products = iapManager.suggestedProducts()
-        products.insert(.Features.appleTV)
-        return products
     }
 
     func moveModules(from offsets: IndexSet, to newOffset: Int) {

Packages/App/Sources/AppUITV/Views/App/AppCoordinator.swift
@@ -45,7 +45,7 @@ public struct AppCoordinator: View, AppCoordinatorConforming {
     private var paywallReason: PaywallReason?
 
     @State
-    private var onCancelPaywall: (() -> Void)?
+    private var paywallContinuation: (() -> Void)?
 
     @StateObject
     private var interactiveManager = InteractiveManager()
@@ -92,7 +92,9 @@ public struct AppCoordinator: View, AppCoordinatorConforming {
             .navigationDestination(for: AppCoordinatorRoute.self, destination: pushDestination)
             .modifier(PaywallModifier(
                 reason: $paywallReason,
-                onCancel: onCancelPaywall
+                onAction: { _ in
+                    paywallContinuation?()
+                }
             ))
             .withErrorHandler(errorHandler)
         }
@@ -178,7 +180,7 @@ extension AppCoordinator {
     public func onPurchaseRequired(
         for profile: Profile,
         features: Set<AppFeature>,
-        onCancel: (() -> Void)?
+        continuation: (() -> Void)?
     ) {
         pp_log_g(.app, .info, "Purchase required for features: \(features)")
         guard !iapManager.isLoadingReceipt else {
@@ -192,14 +194,14 @@ extension AppCoordinator {
                     "\n\n",
                     V.Connect._2(iapManager.verificationDelayMinutes)
                 ].joined(separator: " "),
-                onDismiss: onCancel
+                onDismiss: continuation
             )
             return
         }
         pp_log_g(.app, .info, "Present paywall")
-        onCancelPaywall = onCancel
+        paywallContinuation = continuation
 
-        setLater(.init(nil, requiredFeatures: features, action: .connect)) {
+        setLater(.init(profile, requiredFeatures: features, action: .connect)) {
             paywallReason = $0
         }
     }

commit e820d63d3a52ee2128d7b6a8dd0b95cd84636db5

Fix PaywallCoordinator model observation
Broken in #1422

Packages/App/Sources/UILibrary/Views/Paywall/PaywallCoordinator+Model.swift
@@ -32,6 +32,8 @@ extension PaywallCoordinator {
 
     @MainActor
     final class Model: ObservableObject {
+
+        @Published
         var isFetchingProducts = true
 
         private(set) var suggestedProducts: Set<AppProduct> = []
@@ -40,8 +42,10 @@ extension PaywallCoordinator {
 
         private(set) var individualPurchasable: [InAppProduct] = []
 
+        @Published
         var purchasingIdentifier: String?
 
+        @Published
         var isPurchasePendingConfirmation = false
     }
 }
@@ -94,6 +98,7 @@ extension PaywallCoordinator.Model {
             throw AppError.emptyProducts
         }
 
+        objectWillChange.send()
         self.suggestedProducts = suggestedProducts
         self.completePurchasable = completePurchasable
         self.individualPurchasable = individualPurchasable

Packages/App/Sources/UILibrary/Views/Paywall/PaywallCoordinator.swift
@@ -38,7 +38,7 @@ struct PaywallCoordinator: View {
 
     let requiredFeatures: Set<AppFeature>
 
-    @State
+    @StateObject
     private var model = Model()
 
     @StateObject
@@ -73,7 +73,10 @@ private extension PaywallCoordinator {
 #if os(tvOS)
         PaywallView(
             requiredFeatures: requiredFeatures,
-            state: state
+            model: model
+            // errorHandler
+            // onComplete
+            // onError
         )
         .themeNavigationStack()
 #else
@@ -81,7 +84,7 @@ private extension PaywallCoordinator {
             isPresented: $isPresented,
             iapManager: iapManager,
             requiredFeatures: requiredFeatures,
-            model: $model,
+            model: model,
             errorHandler: errorHandler,
             onComplete: onComplete,
             onError: onError

Packages/App/Sources/UILibrary/Views/Paywall/PaywallView+Main.swift
@@ -40,7 +40,7 @@ struct PaywallView: View {
 
     let requiredFeatures: Set<AppFeature>
 
-    @Binding
+    @ObservedObject
     var model: PaywallCoordinator.Model
 
     @ObservedObject
@@ -143,7 +143,7 @@ private extension PaywallView {
         isPresented: .constant(true),
         iapManager: .forPreviews,
         requiredFeatures: features,
-        model: .constant(.forPreviews(features, including: [.complete])),
+        model: .forPreviews(features, including: [.complete]),
         errorHandler: .default(),
         onComplete: { _, _ in },
         onError: { _ in }