passepartoutvpn/passepartout

commit d8d25ea6382cb8ba87b860054e87e68a2ecabd3f

Extend paywall with individual purchases (#1422)
We want to offer a "Purchase" button in the purchase confirmation alert,
in addition to the "Connect" and "Save anyway" fallback actions. This is
problematic in case the profile requires the purchase of more than 1
product, because StoreKit only allows 1 purchase at a time.

Therefore, split the paywall into two sections:

- Complete packages (if eligible)
- Individual purchases

In the case of individual purchases, let the user purchase them one at a
time. Gray out the purchased ones, and dismiss the paywall after
purchasing all of them.

The PR prepares for the reintroduction of the paywall on tvOS, but this
will be done later (it crashes now). For now, share the paywall view
logic in PaywallCoordinator and split the iOS/macOS (+Main) and tvOS
(+TV) layouts.

Packages/App/Sources/UILibrary/Views/Paywall/PaywallModifier.swift
@@ -34,9 +34,7 @@ public struct PaywallModifier: ViewModifier {
     @Binding
     private var reason: PaywallReason?
 
-    private let otherTitle: String?
-
-    private let onOtherAction: ((Profile?) -> Void)?
+    private let onAction: ((Profile?) -> Void)?
 
     private let onCancel: (() -> Void)?
 
@@ -48,13 +46,11 @@ public struct PaywallModifier: ViewModifier {
 
     public init(
         reason: Binding<PaywallReason?>,
-        otherTitle: String? = nil,
-        onOtherAction: ((Profile?) -> Void)? = nil,
+        onAction: ((Profile?) -> Void)? = nil,
         onCancel: (() -> Void)? = nil
     ) {
         _reason = reason
-        self.otherTitle = otherTitle
-        self.onOtherAction = onOtherAction
+        self.onAction = onAction
         self.onCancel = onCancel
     }
 
@@ -112,19 +108,29 @@ private extension PaywallModifier {
 // MARK: - Confirmation alert
 
 private extension PaywallModifier {
-
-    @ViewBuilder
-    func confirmationActions() -> some View {
-#if !os(tvOS)
-        if !iapManager.isBeta, let otherTitle, let onOtherAction {
-            Button(otherTitle) {
-                onOtherAction(reason?.profile)
-            }
+    func title(forAction action: Action) -> String {
+        switch action {
+        case .connect:
+            return Strings.Global.Actions.connect
+        case .purchase:
+            return Strings.Global.Actions.purchase
+        case .save:
+            return Strings.Views.Paywall.Alerts.Actions.save
         }
-#endif
-        Button(Strings.Global.Nouns.ok, role: .cancel) {
-            reason = nil
-            onCancel?()
+    }
+
+    func confirmationActions() -> some View {
+        reason.map { reason in
+            Group {
+                if !iapManager.isBeta, let onAction {
+                    Button(title(forAction: reason.action), role: .cancel) {
+                        onAction(reason.profile)
+                    }
+                }
+                Button(Strings.Global.Actions.purchase) {
+                    isPurchasing = true
+                }
+            }
         }
     }
 
@@ -145,8 +151,6 @@ private extension PaywallModifier {
         switch reason?.action {
         case .connect:
             messages.append(V.Message.connect(limitedMinutes))
-        case .save:
-            messages.append(V.Message.save)
         default:
             break
         }
@@ -187,18 +191,12 @@ private extension PaywallModifier {
 
 private extension PaywallModifier {
     func modalDestination() -> some View {
-#if !os(tvOS)
         reason.map {
-            PaywallView(
+            PaywallCoordinator(
                 isPresented: $isPurchasing,
-                requiredFeatures: iapManager.excludingEligible(from: $0.requiredFeatures),
-                suggestedProducts: $0.suggestedProducts
+                requiredFeatures: iapManager.excludingEligible(from: $0.requiredFeatures)
             )
-            .themeNavigationStack()
         }
-#else
-        fatalError("tvOS: Paywall unsupported")
-#endif
     }
 }
 

Packages/App/Sources/AppUIMain/Views/App/AppCoordinator.swift
@@ -60,7 +60,7 @@ public struct AppCoordinator: View, AppCoordinatorConforming, SizeClassProviding
     private var paywallReason: PaywallReason?
 
     @State
-    private var onCancelPaywall: (() -> Void)?
+    private var paywallContinuation: (() -> Void)?
 
     @State
     private var modalRoute: ModalRoute?
@@ -102,14 +102,9 @@ public struct AppCoordinator: View, AppCoordinatorConforming, SizeClassProviding
         ))
         .modifier(PaywallModifier(
             reason: $paywallReason,
-            otherTitle: Strings.Views.Paywall.Alerts.Confirmation.editProfile,
-            onOtherAction: { profile in
-                guard let profile else {
-                    return
-                }
-                onEditProfile(profile.localizedPreview)
-            },
-            onCancel: onCancelPaywall
+            onAction: { _ in
+                paywallContinuation?()
+            }
         ))
         .themeModal(
             item: $modalRoute,
@@ -301,7 +296,7 @@ extension AppCoordinator {
     public func onPurchaseRequired(
         for profile: Profile,
         features: Set<AppFeature>,
-        onCancel: (() -> Void)?
+        continuation: (() -> Void)?
     ) {
         pp_log_g(.app, .info, "Purchase required for features: \(features)")
         guard !iapManager.isLoadingReceipt else {
@@ -315,12 +310,12 @@ extension AppCoordinator {
                     "\n\n",
                     V.Connect._2(iapManager.verificationDelayMinutes)
                 ].joined(separator: " "),
-                onDismiss: onCancel
+                onDismiss: continuation
             )
             return
         }
         pp_log_g(.app, .info, "Present paywall")
-        onCancelPaywall = onCancel
+        paywallContinuation = continuation
 
         setLater(.init(profile, requiredFeatures: features, action: .connect)) {
             paywallReason = $0

Packages/App/Sources/AppUIMain/Views/Profile/ProfileCoordinator.swift
@@ -71,7 +71,12 @@ struct ProfileCoordinator: View {
 
     var body: some View {
         contentView
-            .modifier(PaywallModifier(reason: $paywallReason))
+            .modifier(PaywallModifier(
+                reason: $paywallReason,
+                onAction: { _ in
+                    saveAnyway()
+                }
+            ))
             .themeModal(item: $modalRoute, content: modalDestination)
             .environment(\.dismissProfile, onDismiss)
             .withErrorHandler(errorHandler)
@@ -151,6 +156,12 @@ private extension ProfileCoordinator {
 // MARK: - Actions
 
 private extension ProfileCoordinator {
+    func saveAnyway() {
+        Task {
+            try await commitEditing(verifying: false, dismissing: true)
+        }
+    }
+
     func addNewModule(_ moduleType: ModuleType) {
         let module = moduleType.newModule(with: registry)
         withAnimation(theme.animation(for: .modules)) {
@@ -195,7 +206,6 @@ private extension ProfileCoordinator {
             setLater(PaywallReason(
                 nil,
                 requiredFeatures: requiredFeatures,
-                suggestedProducts: nil,
                 action: .save
             )) {
                 paywallReason = $0

Packages/App/Sources/AppUIMain/Views/Profile/ProfileStorageSection.swift
@@ -73,12 +73,7 @@ private extension ProfileStorageSection {
     var sharingPurchaseButton: some View {
         PurchaseRequiredView(
             requiring: sharingRequirements,
-            reason: $paywallReason,
-            suggesting: {
-                var products = iapManager.suggestedProducts()
-                products.insert(.Features.appleTV)
-                return products
-            }()
+            reason: $paywallReason
         )
     }
 
@@ -96,12 +91,7 @@ private extension ProfileStorageSection {
     var tvPurchaseButton: some View {
         PurchaseRequiredView(
             requiring: tvRequirements,
-            reason: $paywallReason,
-            suggesting: {
-                var products = iapManager.suggestedProducts(filter: .onlyComplete)
-                products.insert(.Features.appleTV)
-                return products
-            }()
+            reason: $paywallReason
         )
     }
 }

Packages/App/Sources/AppUIMain/Views/Profile/macOS/ModuleListView+macOS.swift
@@ -64,8 +64,7 @@ struct ModuleListView: View, Routable {
                         Text(Strings.Global.Nouns.general)
                         PurchaseRequiredView(
                             requiring: requiredGeneralFeatures,
-                            reason: $paywallReason,
-                            suggesting: suggestedGeneralProducts
+                            reason: $paywallReason
                         )
                     }
                 }
@@ -146,12 +145,6 @@ private extension ModuleListView {
             features.insert(.sharing)
         }
         return features
-    }
-
-    var suggestedGeneralProducts: Set<AppProduct> {
-        var products = iapManager.suggestedProducts()
-        products.insert(.Features.appleTV)
-        return products
     }
 
     func moveModules(from offsets: IndexSet, to newOffset: Int) {

Packages/App/Sources/AppUITV/Views/App/AppCoordinator.swift
@@ -45,7 +45,7 @@ public struct AppCoordinator: View, AppCoordinatorConforming {
     private var paywallReason: PaywallReason?
 
     @State
-    private var onCancelPaywall: (() -> Void)?
+    private var paywallContinuation: (() -> Void)?
 
     @StateObject
     private var interactiveManager = InteractiveManager()
@@ -92,7 +92,9 @@ public struct AppCoordinator: View, AppCoordinatorConforming {
             .navigationDestination(for: AppCoordinatorRoute.self, destination: pushDestination)
             .modifier(PaywallModifier(
                 reason: $paywallReason,
-                onCancel: onCancelPaywall
+                onAction: { _ in
+                    paywallContinuation?()
+                }
             ))
             .withErrorHandler(errorHandler)
         }
@@ -178,7 +180,7 @@ extension AppCoordinator {
     public func onPurchaseRequired(
         for profile: Profile,
         features: Set<AppFeature>,
-        onCancel: (() -> Void)?
+        continuation: (() -> Void)?
     ) {
         pp_log_g(.app, .info, "Purchase required for features: \(features)")
         guard !iapManager.isLoadingReceipt else {
@@ -192,14 +194,14 @@ extension AppCoordinator {
                     "\n\n",
                     V.Connect._2(iapManager.verificationDelayMinutes)
                 ].joined(separator: " "),
-                onDismiss: onCancel
+                onDismiss: continuation
             )
             return
         }
         pp_log_g(.app, .info, "Present paywall")
-        onCancelPaywall = onCancel
+        paywallContinuation = continuation
 
-        setLater(.init(nil, requiredFeatures: features, action: .connect)) {
+        setLater(.init(profile, requiredFeatures: features, action: .connect)) {
             paywallReason = $0
         }
     }

Packages/App/Sources/CommonIAP/Domain/AppFeature.swift
@@ -44,7 +44,7 @@ public enum AppFeature: String, CaseIterable {
 }
 
 extension AppFeature {
-    public static let essentialFeatures: [AppFeature] = [
+    public static let essentialFeatures: Set<AppFeature> = [
         .dns,
         .httpProxy,
         .onDemand,
@@ -53,6 +53,10 @@ extension AppFeature {
         .routing,
         .sharing
     ]
+
+    public var isEssential: Bool {
+        Self.essentialFeatures.contains(self)
+    }
 }
 
 extension AppFeature: Identifiable {

Packages/App/Sources/CommonIAP/Strategy/FakeAppProductHelper.swift
@@ -54,12 +54,7 @@ public actor FakeAppProductHelper: AppProductHelper {
 
     public func fetchProducts(timeout: Int) async throws -> [AppProduct: InAppProduct] {
         products = AppProduct.all.reduce(into: [:]) {
-            $0[$1] = InAppProduct(
-                productIdentifier: $1.rawValue,
-                localizedTitle: $1.rawValue,
-                localizedPrice: "€10.0",
-                native: $1
-            )
+            $0[$1] = $1.asFakeIAP
         }
         await receiptReader.setReceipt(withPurchase: purchase, identifiers: [])
         didUpdateSubject.send()
@@ -74,5 +69,16 @@ public actor FakeAppProductHelper: AppProductHelper {
 
     public func restorePurchases() async throws {
         didUpdateSubject.send()
+    }
+}
+
+extension AppProduct {
+    public var asFakeIAP: InAppProduct {
+        InAppProduct(
+            productIdentifier: rawValue,
+            localizedTitle: rawValue,
+            localizedPrice: "€10.0",
+            native: self
+        )
     }
 }

commit 2957eb4613bb4fe286d0945e680bd148511202db

Omit "Purchase" from beta
Regression in #1422

Packages/App/Sources/UILibrary/Views/Paywall/PaywallModifier.swift
@@ -122,13 +122,15 @@ private extension PaywallModifier {
     func confirmationActions() -> some View {
         reason.map { reason in
             Group {
-                if !iapManager.isBeta, let onAction {
+                if let onAction {
                     Button(title(forAction: reason.action), role: .cancel) {
                         onAction(reason.profile)
                     }
                 }
-                Button(Strings.Global.Actions.purchase) {
-                    isPurchasing = true
+                if !iapManager.isBeta {
+                    Button(Strings.Global.Actions.purchase) {
+                        isPurchasing = true
+                    }
                 }
             }
         }