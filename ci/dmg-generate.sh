9543050b743d29bbccd8c3d0bde5923e3176cab2
Davide <keeshux@gmail.com>
2025-07-09 16:26:07 +0200

    Beautify the layout of .dmg in Finder (#1461)
    Add a "template mode" to the dmg-generate script to create a r/w image.
    The template image is then used to produce the .DS_Store that will be
    applied to the final, read-only image.
    
    Visual tweaks:
    
    - Scale and align
    - Increase font size
    - Add drag background
    - Hide sidebar and toolbars
    
    Fixes #1444

--- ci/dmg-generate.sh
+++ ci/dmg-generate.sh
@@ -2,6 +2,7 @@
 cwd=`dirname $0`
 name="Passepartout"
 arch="$1"
+is_template="$2"
 if [[ -z "$arch" ]]; then
     echo "Missing arch"
     exit 1
@@ -10,16 +11,40 @@ fi
 version=`$cwd/version-number.sh`
 volname="$name $version $arch"
 srcfolder="$cwd/dmg"
-dmg="$name.$arch.dmg"
+dmg="$name.$arch"
 
 set -e
 
 echo "Copy .app to .dmg contents..."
 cp -RH "dist/macOS/$name.app" "$srcfolder"
 
+if [[ -n "$is_template" ]]; then
+    echo "Create template $volname..."
+    hdiutil create \
+        -volname "$volname" \
+        -srcfolder "$srcfolder" \
+        -format UDRW \
+        -ov \
+        "$dmg.template"
+
+    echo "Mount template $volname..."
+    mnt="/Volumes/$volname"
+    hdiutil attach "$dmg.template.dmg" \
+        -mountpoint "$mnt" \
+        -readwrite -noautoopen
+
+    echo "Reapply .DS_Store..."
+    cp "$srcfolder/.DS_Store" "$mnt"
+    chmod 644 "$mnt/.DS_Store"
+
+    # stop at template to edit .DS_Store
+    exit
+fi
+
 echo "Create $volname..."
 hdiutil create \
     -volname "$volname" \
     -srcfolder "$srcfolder" \
-    -ov -format UDZO \
+    -format UDZO \
+    -ov \
     "$dmg"
