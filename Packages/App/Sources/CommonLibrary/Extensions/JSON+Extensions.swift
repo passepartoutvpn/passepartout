2ff87a3210fac94be4aa0168ccce1f9c99d51cf4
Davide <keeshux@gmail.com>
2025-07-20 23:24:53 +0200

    Fix missing Chinese translations (#1477)
    Resolve parsing errors due to missing trailing semicolons. Also fix
    language code in ConfigBundle.

--- Packages/App/Sources/CommonLibrary/Extensions/JSON+Extensions.swift
+++ Packages/App/Sources/CommonLibrary/Extensions/JSON+Extensions.swift
@@ -27,9 +27,9 @@ import Foundation
 import GenericJSON
 
 extension JSON {
-    public func string(forKey key: String) -> String {
+    public func localizedString(forKey key: String) -> String {
         let dict = objectValue?[key]
         let code = Locale.current.language.languageCode?.identifier ?? "en"
-        return dict?[code]?.stringValue ?? key
+        return dict?[code]?.stringValue ?? dict?["en"]?.stringValue ?? key
     }
 }
