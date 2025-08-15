1afde79c3155c9ff79a1dcf5d3fb3f51a3ade069
Davide <keeshux@gmail.com>
2025-08-15 15:32:32 +0200

    Fix Core source CI script

--- ci/use-partout-core-source.sh
+++ ci/use-partout-core-source.sh
@@ -3,8 +3,8 @@ manifest="submodules/partout/Package.swift"
 core_status=`git submodule status submodules/partout-core`
 sha1=${core_status:1:40}
 
-env_line_old='let coreDeployment = packageCoreDeployment \?\? \.remoteBinary'
-env_line_new='let coreDeployment = packageCoreDeployment \?\? .remoteSource'
+env_line_old='let coreDeployment = envCoreDeployment \?\? \.remoteBinary'
+env_line_new='let coreDeployment = envCoreDeployment \?\? .remoteSource'
 sed -E -i '' "s/^${env_line_old}$/${env_line_new}/" "$manifest"
 if ! grep -E -q "^${env_line_new}$" "$manifest"; then
     echo "Unable to set Core deployment"
@@ -13,7 +13,7 @@ fi
 
 sha1_line_pattern="let coreSourceSHA1 = .*"
 sha1_line_new="let coreSourceSHA1 = \"${sha1}\""
-sed -i '' "s/^${sha1_line_pattern}$/${sha1_line_new}/" "$manifest"
+sed -E -i '' "s/^${sha1_line_pattern}$/${sha1_line_new}/" "$manifest"
 if ! grep -E -q "^${sha1_line_new}$" "$manifest"; then
     echo "Unable to set SHA-1"
     exit 1
