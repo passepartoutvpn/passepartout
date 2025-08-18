b20f3ea6bab44f2d77e9b9717cd731f01506c04d
Davide <keeshux@gmail.com>
2025-08-19 00:48:51 +0200

    Use SHA1 suffix in folder
    So that .so files in APK are unversioned.

--- app-android/scripts/pull-libraries.sh
+++ app-android/scripts/pull-libraries.sh
@@ -2,7 +2,6 @@
 partout_path="../submodules/partout"
 cpp_path="app/src/main/cpp"
 headers_path="$cpp_path"
-libs_path="$cpp_path/libs/partout/arm64-v8a"
 
 set -e
 pushd $partout_path
@@ -11,9 +10,10 @@ scripts/build-android.sh 1  # 1 for Release
 popd
 
 rm -f $headers_path/partout.h
-rm -rf $libs_path
-mkdir -p $libs_path
+rm -rf $cpp_path/libs/partout-*
 
+libs_path="$cpp_path/libs/partout-${partout_sha1}/arm64-v8a"
+mkdir -p $libs_path
 cp $partout_path/Sources/ABI/Library_C/include/partout.h $headers_path
-cp $partout_path/.build/release/libPartout.so $libs_path/libPartout-$partout_sha1.so
-cp $partout_path/.build/release/libPartoutImplementations.so $libs_path/libPartoutImplementations-$partout_sha1.so
+cp $partout_path/.build/release/libPartout.so $libs_path/libPartout.so
+cp $partout_path/.build/release/libPartoutImplementations.so $libs_path/libPartoutImplementations.so
